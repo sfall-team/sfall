535b7edc55ae4b76fee4c139a5af625e5d50de58
NovaRain <novarain@gmail.com>
2017-02-12 00:11:21 +0800

    Limited the range of values for StartYear/Month/Day to prevent errors when setting them to negative values, and added a clarification in ddraw.ini.

--- sfall/main.cpp
+++ sfall/main.cpp
@@ -970,20 +970,20 @@ static void DllMain2() {
 	SafeWrite32(0x00418B6D, (DWORD)&dfModelName);
 	dlogr(" Done", DL_INIT);
 
-	int date=GetPrivateProfileInt("Misc", "StartYear", -1, ini);
-	if(date!=-1) {
+	int date = GetPrivateProfileInt("Misc", "StartYear", -1, ini);
+	if (date >= 0) {
 		dlog("Applying starting year patch.", DL_INIT);
 		SafeWrite32(0x4A336C, date);
 		dlogr(" Done", DL_INIT);
 	}
-	date=GetPrivateProfileInt("Misc", "StartMonth", -1, ini);
-	if(date!=-1) {
+	date = GetPrivateProfileInt("Misc", "StartMonth", -1, ini);
+	if (date >= 0 && date < 12) {
 		dlog("Applying starting month patch.", DL_INIT);
 		SafeWrite32(0x4A3382, date);
 		dlogr(" Done", DL_INIT);
 	}
-	date=GetPrivateProfileInt("Misc", "StartDay", -1, ini);
-	if(date!=-1) {
+	date = GetPrivateProfileInt("Misc", "StartDay", -1, ini);
+	if (date >= 0 && date < 31) {
 		dlog("Applying starting day patch.", DL_INIT);
 		SafeWrite8(0x4A3356, date);
 		dlogr(" Done", DL_INIT);
