81d3479bff94fd208d17d3e88e9474ecd1d1a82a
phobos2077 <phobos2077@gmail.com>
2017-03-11 15:43:28 +0700

    Greatly increase compilation speed using precompiled header

---
+++ sfall/stdafx.h
@@ -0,0 +1,9 @@
+#pragma message("Compiling precompiled headers.\n")
+
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <Windows.h>
