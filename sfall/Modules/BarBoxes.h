e260d551079a94688f6c4926f3ae0c7b3d817743
Mr.Stalin <fakels@yandex.ru>
2018-07-14 04:08:00 +0300

    Added option "BarBoxesCount" allows to specify the count of used custom bar boxes in game.

--- sfall/Modules/BarBoxes.h
+++ sfall/Modules/BarBoxes.h
@@ -4,15 +4,21 @@ namespace sfall
 {
 
 class BarBoxes : public Module {
+private:
+	static int boxCount;
+
 public:
 	const char* name() { return "BarBoxes"; }
 	void init();
+	void exit() override;
 
+	static int  MaxBox() { return boxCount - 1; } 
 	static void SetText(int box, const char* text, DWORD color);
-};
 
-int _stdcall GetBox(int i);
-void _stdcall AddBox(int i);
-void _stdcall RemoveBox(int i);
+	static bool GetBox(int);
+	static void AddBox(int);
+	static void RemoveBox(int);
+
+};
 
 }
