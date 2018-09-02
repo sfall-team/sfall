005eb201fa91832c07a545d9503ff09c9c5e1e06
Mr.Stalin <fakels@yandex.ru>
2018-09-02 23:06:47 +0300

    To the configuration file 'Elevators.ini' options been added, allowing change the frm-image of the elevator control panel.

--- sfall/Modules/Elevators.cpp
+++ sfall/Modules/Elevators.cpp
@@ -31,6 +31,7 @@ static char elevFile[MAX_PATH];
 
 static fo::ElevatorExit elevatorExits[elevatorCount][exitsPerElevator];
 static DWORD menus[elevatorCount];
+static fo::ElevatorFrms elevatorsFrms[elevatorCount];
 
 static void __declspec(naked) GetMenuHook() {
 	__asm {
@@ -73,7 +74,6 @@ static void __declspec(naked) GetNumButtonsHook1() {
 		lea  esi, menus;
 		mov  eax, [esi+edi*4];
 		mov  eax, [FO_VAR_btncnt + eax*4];
-		push 0x43F064;
 		retn;
 	}
 }
@@ -83,7 +83,6 @@ static void __declspec(naked) GetNumButtonsHook2() {
 		lea  edx, menus;
 		mov  eax, [edx+edi*4];
 		mov  eax, [FO_VAR_btncnt + eax*4];
-		push 0x43F18B;
 		retn;
 	}
 }
@@ -93,7 +92,6 @@ static void __declspec(naked) GetNumButtonsHook3() {
 		lea  eax, menus;
 		mov  eax, [eax+edi*4];
 		mov  eax, [FO_VAR_btncnt+eax*4];
-		push 0x43F1EB;
 		retn;
 	}
 }
@@ -101,13 +99,20 @@ static void __declspec(naked) GetNumButtonsHook3() {
 void ResetElevators() {
 	memcpy(elevatorExits, fo::var::retvals, sizeof(fo::ElevatorExit) * vanillaElevatorCount * exitsPerElevator);
 	memset(&elevatorExits[vanillaElevatorCount], 0, sizeof(fo::ElevatorExit) * (elevatorCount - vanillaElevatorCount) * exitsPerElevator);
+
+	memcpy(elevatorsFrms, (void*)FO_VAR_intotal, sizeof(fo::ElevatorFrms) * vanillaElevatorCount);
+	memset(&elevatorsFrms[vanillaElevatorCount], 0, sizeof(fo::ElevatorFrms) * (elevatorCount - vanillaElevatorCount));
+
 	for (int i = 0; i < vanillaElevatorCount; i++) menus[i] = i;
 	for (int i = vanillaElevatorCount; i < elevatorCount; i++) menus[i] = 0;
+
 	char section[4];
 	if (elevFile) {
 		for (int i = 0; i < elevatorCount; i++) {
 			_itoa_s(i, section, 10);
 			menus[i] = GetPrivateProfileIntA(section, "Image", menus[i], elevFile);
+			elevatorsFrms[i].main = GetPrivateProfileIntA(section, "MainFrm", elevatorsFrms[i].main, elevFile);
+			elevatorsFrms[i].buttons = GetPrivateProfileIntA(section, "ButtonsFrm", elevatorsFrms[i].buttons, elevFile);
 			char setting[32];
 			for (int j = 0; j < exitsPerElevator; j++) {
 				sprintf_s(setting, "ID%d", j + 1);
@@ -124,9 +129,11 @@ void ResetElevators() {
 void ElevatorsInit(const char* file) {
 	strcpy_s(elevFile, ".\\");
 	strcat_s(elevFile, file);
+
 	HookCall(0x43EF83, GetMenuHook);
 	HookCall(0x43F141, UnknownHook);
 	HookCall(0x43F2D2, UnknownHook2);
+
 	SafeWrite8(0x43EF76, (BYTE)elevatorCount);
 	SafeWrite32(0x43EFA4, (DWORD)elevatorExits);
 	SafeWrite32(0x43EFB9, (DWORD)elevatorExits);
@@ -135,12 +142,13 @@ void ElevatorsInit(const char* file) {
 	SafeWrite32(0x43F309, (DWORD)&elevatorExits[0][0].elevation);
 	SafeWrite32(0x43F315, (DWORD)&elevatorExits[0][0].tile);
 
-	SafeWrite8(0x43F05D, 0xe9);
-	HookCall(0x43F05D, GetNumButtonsHook1);
-	SafeWrite8(0x43F184, 0xe9);
-	HookCall(0x43F184, GetNumButtonsHook2);
-	SafeWrite8(0x43F1E4, 0xe9);
-	HookCall(0x43F1E4, GetNumButtonsHook3);
+	SafeWrite32(0x43F438, (DWORD)&elevatorsFrms[0].main);
+	SafeWrite32(0x43F475, (DWORD)&elevatorsFrms[0].buttons);
+
+	MakeCall(0x43F05D, GetNumButtonsHook1, 2);
+	MakeCall(0x43F184, GetNumButtonsHook2, 2);
+	MakeCall(0x43F1E4, GetNumButtonsHook3, 2);
+
 	ResetElevators();
 }
 
