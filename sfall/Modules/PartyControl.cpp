bceb50502414701f62370cca7d5e6abf7f6ec0d7
NovaRain <novarain@gmail.com>
2018-10-16 10:05:25 +0800

    Added displaying party member's current level & AC & addict flag info to the combat control panel (from Mr.Stalin)
    Changed highlighting containers/corpses to be default on in sfall-mods.ini.

--- sfall/Modules/PartyControl.cpp
+++ sfall/Modules/PartyControl.cpp
@@ -230,11 +230,54 @@ fo::GameObject* PartyControl::RealDudeObject() {
 		: fo::var::obj_dude;
 }
 
+static char levelMsg[12], armorClassMsg[12], addictMsg[16];
+static void __fastcall PartyMemberPrintStat(BYTE* surface, DWORD toWidth) {
+	const char* fmt = "%s %d";
+	char lvlMsg[16], acMsg[16];
+
+	fo::GameObject* partyMember = (fo::GameObject*)fo::var::dialog_target;
+	int xPos = 350;
+
+	int level = fo::func::partyMemberGetCurLevel(partyMember);
+	sprintf_s(lvlMsg, fmt, levelMsg, level);
+
+	BYTE color = fo::var::GreenColor;
+	int widthText = fo::GetTextWidth(lvlMsg);
+	fo::PrintText(lvlMsg, color, xPos - widthText, 96, widthText, toWidth, surface);
+
+	int ac = fo::func::stat_level(partyMember, fo::STAT_ac);
+	sprintf_s(acMsg, fmt, armorClassMsg, ac);
+
+	xPos -= fo::GetTextWidth(armorClassMsg) + 20;
+	fo::PrintText(acMsg, color, xPos, 167, fo::GetTextWidth(acMsg), toWidth, surface);
+
+	color = (fo::func::queue_find_first(partyMember, 2)) ? fo::var::RedColor : fo::var::DarkGreenColor;
+	widthText = fo::GetTextWidth(addictMsg);
+	fo::PrintText(addictMsg, color, 350 - widthText, 148, widthText, toWidth, surface);
+}
+
+static void __declspec(naked) gdControlUpdateInfo_hook() {
+	__asm {
+		mov  edi, eax; // keep fontnum
+		mov  ecx, ebp;
+		mov  edx, esi;
+		call PartyMemberPrintStat;
+		mov  eax, edi;
+		jmp  fo::funcoffs::text_font_;
+	}
+}
+
 void PartyControl::init() {
 	LoadGameHook::OnGameReset() += PartyControlReset;
 
 	HookCall(0x454218, stat_pc_add_experience_hook); // call inside op_give_exp_points_hook
 	HookCalls(pc_flag_toggle_hook, { 0x4124F1, 0x41279A });
+
+	// display party member's current level & AC & addict flag
+	HookCall(0x44926F, gdControlUpdateInfo_hook);
+	Translate("sfall", "PartyLvlMsg", "Lvl:", levelMsg, 12);
+	Translate("sfall", "PartyACMsg", "AC:", armorClassMsg, 12);
+	Translate("sfall", "PartyAddictMsg", "Addict", addictMsg, 16);
 }
 
 }
