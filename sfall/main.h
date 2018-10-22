20da0a1897198e3559a12aaf602b84ab91f451b5
Mr.Stalin <fakels@yandex.ru>
2018-10-22 20:21:13 +0300

    Added missing functions from the source library ddraw.dll to export from sfall.

--- sfall/main.h
+++ sfall/main.h
@@ -26,6 +26,32 @@
 #include "SafeWrite.h"
 #include "Logging.h"
 
+struct ddrawDll {
+	HMODULE dll;
+	FARPROC	DirectDrawEnumerateExA;
+	FARPROC DirectDrawCreate;
+	FARPROC	DirectDrawCreateEx;
+	FARPROC	AcquireDDThreadLock;
+	FARPROC	ReleaseDDThreadLock;
+	FARPROC CheckFullscreen;
+	FARPROC CompleteCreateSysmemSurface;
+	FARPROC D3DParseUnknownCommand;
+	FARPROC DDGetAttachedSurfaceLcl;
+	FARPROC DDInternalLock;
+	FARPROC DDInternalUnlock;
+	FARPROC DSoundHelp;
+	FARPROC DirectDrawCreateClipper;
+	FARPROC DirectDrawEnumerateA;
+	FARPROC DirectDrawEnumerateExW;
+	FARPROC DirectDrawEnumerateW;
+	//FARPROC DllCanUnloadNow;
+	//FARPROC DllGetClassObject;
+	FARPROC GetDDSurfaceLocal;
+	FARPROC GetOLEThunkData;
+	FARPROC GetSurfaceFromDC;
+	FARPROC RegisterSpecialCase;
+};
+
 namespace sfall
 {
 
