f36bd6fb618ba6fbbcafb8597ef1f9cbcc72d496
NovaRain <novarain@gmail.com>
2018-12-31 10:10:06 +0800

    Improved the functionality of ElevatorsFile to allow to change the FRM images of the elevator panel and create new elevator types (from Mr.Stalin)

--- sfall/Elevators.cpp
+++ sfall/Elevators.cpp
@@ -21,8 +21,11 @@
 #include "Elevators.h"
 #include "FalloutEngine.h"
 
-static const int ElevatorCount = 50;
-static char File[MAX_PATH];
+static const int exitsPerElevator = 4;
+static const int vanillaElevatorCount = 24;
+static const int elevatorCount = 50;
+
+static char elevFile[MAX_PATH] = ".\\";
 
 struct sElevator {
 	DWORD ID1;
@@ -39,119 +42,143 @@ struct sElevator {
 	DWORD Tile4;
 };
 
-static sElevator Elevators[ElevatorCount];
-static DWORD Menus[ElevatorCount];
+struct sElevatorFrms {
+	DWORD main;
+	DWORD buttons;
+};
 
-void SetElevator(DWORD id, DWORD index, DWORD value) {
-	if (id >= ElevatorCount || index >= 12) return;
-	*(DWORD*)(((DWORD)&Elevators[id]) + index * 4) = value;
-}
+static DWORD elevatorType[elevatorCount];
+static sElevator elevators[elevatorCount];         // _retvals
+static sElevatorFrms elevatorsFrms[elevatorCount]; // _intotal
+static DWORD elevatorsBtnCount[elevatorCount];     // _btncount
 
 static void __declspec(naked) GetMenuHook() {
 	__asm {
-		push ebx;
-		lea ebx, Menus;
-		shl eax, 2;
-		mov eax, [ebx+eax];
-		call elevator_start_;
-		pop ebx;
-		ret;
+		lea  edx, elevatorType;
+		shl  eax, 2;
+		mov  eax, [edx + eax];
+		jmp  elevator_start_;
 	}
 }
 
-static void __declspec(naked) UnknownHook() {
+static void __declspec(naked) CheckHotKeysHook() {
 	__asm {
+		cmp  eax, vanillaElevatorCount;
+		jge  skip; // skip hotkeys data
 		push ebx;
-		lea ebx, Menus;
-		shl eax, 2;
-		mov eax, [ebx+eax];
+		lea  ebx, elevatorType;
+		shl  eax, 2;
+		mov  eax, [ebx + eax];
 		call Check4Keys_;
-		pop ebx;
-		ret;
+		pop  ebx;
+		retn;
+skip:
+		xor  eax, eax;
+		retn;
 	}
 }
 
+/*
 static void __declspec(naked) UnknownHook2() {
 	__asm {
-		push ebx;
-		lea ebx, Menus;
-		shl eax, 2;
-		mov eax, [ebx+eax];
-		call elevator_end_;
-		pop ebx;
-		ret;
+		lea  edx, elevatorType;
+		shl  eax, 2;
+		mov  eax, [edx + eax];
+		jmp  elevator_end_;
 	}
 }
+*/
 
 static void __declspec(naked) GetNumButtonsHook1() {
 	__asm {
-		lea  esi, Menus;
-		mov  eax, [esi+edi*4];
-		mov  eax, [_btncnt+eax*4];
+		lea  esi, elevatorType;
+		mov  eax, [esi + edi * 4];
+		mov  eax, [elevatorsBtnCount + eax * 4];
 		retn;
 	}
 }
 
 static void __declspec(naked) GetNumButtonsHook2() {
 	__asm {
-		lea  edx, Menus;
-		mov  eax, [edx+edi*4];
-		mov  eax, [_btncnt+eax*4];
+		lea  edx, elevatorType;
+		mov  eax, [edx + edi * 4];
+		mov  eax, [elevatorsBtnCount + eax * 4];
 		retn;
 	}
 }
 
 static void __declspec(naked) GetNumButtonsHook3() {
 	__asm {
-		lea  eax, Menus;
-		mov  eax, [eax+edi*4];
-		mov  eax, [_btncnt+eax*4];
+		lea  eax, elevatorType;
+		mov  eax, [eax + edi * 4];
+		mov  eax, [elevatorsBtnCount + eax * 4];
 		retn;
 	}
 }
 
 void ResetElevators() {
-	memcpy(Elevators, (void*)_retvals, sizeof(sElevator) * 24);
-	memset(&Elevators[24], 0, sizeof(sElevator) * (ElevatorCount - 24));
-	for (int i = 0; i < 24; i++) Menus[i] = i;
-	for (int i = 24; i < ElevatorCount; i++) Menus[i] = 0;
+	memcpy(elevators, (void*)_retvals, sizeof(sElevator) * vanillaElevatorCount);
+	memset(&elevators[vanillaElevatorCount], 0, sizeof(sElevator) * (elevatorCount - vanillaElevatorCount));
+
+	memcpy(elevatorsFrms, (void*)_intotal, sizeof(sElevatorFrms) * vanillaElevatorCount);
+	memset(&elevatorsFrms[vanillaElevatorCount], 0, sizeof(sElevatorFrms) * (elevatorCount - vanillaElevatorCount));
+
+	memcpy(elevatorsBtnCount, (void*)_btncnt, sizeof(DWORD) * vanillaElevatorCount);
+
+	for (int i = 0; i < vanillaElevatorCount; i++) elevatorType[i] = i;
+	for (int i = vanillaElevatorCount; i < elevatorCount; i++) elevatorType[i] = 0;
+
 	char section[4];
-	if (File) {
-		for (int i = 0; i < ElevatorCount; i++) {
+	if (elevFile) {
+		for (int i = 0; i < elevatorCount; i++) {
 			_itoa_s(i, section, 10);
-			Menus[i] = GetPrivateProfileIntA(section, "Image", Menus[i], File);
-			Elevators[i].ID1 = GetPrivateProfileIntA(section, "ID1", Elevators[i].ID1, File);
-			Elevators[i].ID2 = GetPrivateProfileIntA(section, "ID2", Elevators[i].ID2, File);
-			Elevators[i].ID3 = GetPrivateProfileIntA(section, "ID3", Elevators[i].ID3, File);
-			Elevators[i].ID4 = GetPrivateProfileIntA(section, "ID4", Elevators[i].ID4, File);
-			Elevators[i].Elevation1 = GetPrivateProfileIntA(section, "Elevation1", Elevators[i].Elevation1, File);
-			Elevators[i].Elevation2 = GetPrivateProfileIntA(section, "Elevation2", Elevators[i].Elevation2, File);
-			Elevators[i].Elevation3 = GetPrivateProfileIntA(section, "Elevation3", Elevators[i].Elevation3, File);
-			Elevators[i].Elevation4 = GetPrivateProfileIntA(section, "Elevation4", Elevators[i].Elevation4, File);
-			Elevators[i].Tile1 = GetPrivateProfileIntA(section, "Tile1", Elevators[i].Tile1, File);
-			Elevators[i].Tile2 = GetPrivateProfileIntA(section, "Tile2", Elevators[i].Tile2, File);
-			Elevators[i].Tile3 = GetPrivateProfileIntA(section, "Tile3", Elevators[i].Tile3, File);
-			Elevators[i].Tile4 = GetPrivateProfileIntA(section, "Tile4", Elevators[i].Tile4, File);
+			int type = GetPrivateProfileIntA(section, "Image", elevatorType[i], elevFile);
+			elevatorType[i] = min(type, elevatorCount - 1);
+			if (i >= vanillaElevatorCount) {
+				int cBtn = GetPrivateProfileIntA(section, "ButtonCount", 2, elevFile);
+				if (cBtn > exitsPerElevator) cBtn = exitsPerElevator;
+				elevatorsBtnCount[i] = max(2, cBtn);
+			}
+			elevatorsFrms[i].main = GetPrivateProfileIntA(section, "MainFrm", elevatorsFrms[i].main, elevFile);
+			elevatorsFrms[i].buttons = GetPrivateProfileIntA(section, "ButtonsFrm", elevatorsFrms[i].buttons, elevFile);
+			elevatorType[i] = GetPrivateProfileIntA(section, "Image", elevatorType[i], elevFile);
+			elevators[i].ID1 = GetPrivateProfileIntA(section, "ID1", elevators[i].ID1, elevFile);
+			elevators[i].ID2 = GetPrivateProfileIntA(section, "ID2", elevators[i].ID2, elevFile);
+			elevators[i].ID3 = GetPrivateProfileIntA(section, "ID3", elevators[i].ID3, elevFile);
+			elevators[i].ID4 = GetPrivateProfileIntA(section, "ID4", elevators[i].ID4, elevFile);
+			elevators[i].Elevation1 = GetPrivateProfileIntA(section, "Elevation1", elevators[i].Elevation1, elevFile);
+			elevators[i].Elevation2 = GetPrivateProfileIntA(section, "Elevation2", elevators[i].Elevation2, elevFile);
+			elevators[i].Elevation3 = GetPrivateProfileIntA(section, "Elevation3", elevators[i].Elevation3, elevFile);
+			elevators[i].Elevation4 = GetPrivateProfileIntA(section, "Elevation4", elevators[i].Elevation4, elevFile);
+			elevators[i].Tile1 = GetPrivateProfileIntA(section, "Tile1", elevators[i].Tile1, elevFile);
+			elevators[i].Tile2 = GetPrivateProfileIntA(section, "Tile2", elevators[i].Tile2, elevFile);
+			elevators[i].Tile3 = GetPrivateProfileIntA(section, "Tile3", elevators[i].Tile3, elevFile);
+			elevators[i].Tile4 = GetPrivateProfileIntA(section, "Tile4", elevators[i].Tile4, elevFile);
 		}
 	}
 }
 
 void ElevatorsInit(char* file) {
-	strcpy_s(File, ".\\");
-	strcat_s(File, file);
+	strcat_s(elevFile, file);
 
 	HookCall(0x43EF83, GetMenuHook);
-	HookCall(0x43F141, UnknownHook);
-	HookCall(0x43F2D2, UnknownHook2);
+	HookCall(0x43F141, CheckHotKeysHook);
+	//HookCall(0x43F2D2, UnknownHook2); // unused
 
-	SafeWrite8(0x43EF76, (BYTE)ElevatorCount);
-	SafeWrite32(0x43EFA4, (DWORD)Elevators);
-	SafeWrite32(0x43EFB9, (DWORD)Elevators);
-	SafeWrite32(0x43EFEA, (DWORD)&Elevators[0].Tile1);
-	SafeWrite32(0x43F2FC, (DWORD)Elevators);
-	SafeWrite32(0x43F309, (DWORD)&Elevators[0].Elevation1);
-	SafeWrite32(0x43F315, (DWORD)&Elevators[0].Tile1);
+	SafeWrite8(0x43EF76, (BYTE)elevatorCount);
+	SafeWrite32(0x43EFA4, (DWORD)elevators);
+	SafeWrite32(0x43EFB9, (DWORD)elevators);
+	SafeWrite32(0x43F2FC, (DWORD)elevators);
+	SafeWrite32(0x43EFEA, (DWORD)&elevators[0].Tile1);
+	SafeWrite32(0x43F315, (DWORD)&elevators[0].Tile1);
+	SafeWrite32(0x43F309, (DWORD)&elevators[0].Elevation1);
 
+	SafeWrite32(0x43F438, (DWORD)&elevatorsFrms[0].main);
+	SafeWrite32(0x43F475, (DWORD)&elevatorsFrms[0].buttons);
+
+	// _btncnt
+	SafeWrite32(0x43F65E, (DWORD)elevatorsBtnCount);
+	SafeWrite32(0x43F6BB, (DWORD)elevatorsBtnCount);
 	MakeCall(0x43F05D, GetNumButtonsHook1, 2);
 	MakeCall(0x43F184, GetNumButtonsHook2, 2);
 	MakeCall(0x43F1E4, GetNumButtonsHook3, 2);
