08214c00e390ffb22e69ae81d86cc8e51a8abbe7
Mr.Stalin <fakels@yandex.ru>
2019-04-23 21:28:46 +0300

    Update for release v4.1.7

--- sfall/Utils.cpp
+++ sfall/Utils.cpp
@@ -1,9 +1,9 @@
-#include <algorithm> 
+#include <algorithm>
 #include <iterator>
 
 #include "Utils.h"
 
-namespace sfall 
+namespace sfall
 {
 
 std::vector<std::string> split(const std::string &s, char delim) {
@@ -23,6 +23,28 @@ std::string trim(const std::string& str) {
 
 void ToLowerCase(std::string& line) {
 	std::transform(line.begin(), line.end(), line.begin(), ::tolower);
+}
+
+bool isSpace(char c) {
+	return (c == ' ' || c == '\t' /*|| c == '\n' || c == '\r'*/);
+}
+
+void strtrim(char* str) {
+	if (str[0] == 0) return;
+	int len = strlen(str) - 1;
+
+	int i = len;
+	while (len >= 0 && isSpace(str[len])) len--;
+	if (i != len) str[len + 1] = '\0'; // delete all right spaces
+
+	i = 0;
+	while (i < len && isSpace(str[i])) i++;
+	if (i > 0) {
+		int j = 0;
+		do {
+			str[j] = str[j + i]; // shift all chars(include null char) to left
+		} while (++j <= len);
+	}
 }
 
 }
