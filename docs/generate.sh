a016202177b9c2720866150795f0b442bad107e4
Magus <magus@bgforge.net>
2019-06-17 01:22:39 +0700

    disambiguation

--- docs/generate.sh
+++ docs/generate.sh
@@ -3,10 +3,16 @@
 set -xeu -o pipefail
 
 site_dir="docs"
+yml_dir="../artifacts/scripting"
+pages_dir="pages"
+
 pushd .
+
 cd "$site_dir"
-mkdir -p pages
-./yaml_to_md.py ../artifacts/scripting/doc.yml pages
+mkdir -p "$pages_dir"
+./yaml_to_md.py "$yml_dir/functions.yml" "$yml_dir/hooks.yml" "$pages_dir"
+
 bundle install
 bundle exec jekyll build
+
 popd
