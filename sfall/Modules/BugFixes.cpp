997ee71cbb4cac1be78dbd5424d6e2a01fab9bc2
Mr.Stalin <fakels@yandex.ru>
2019-06-25 18:57:08 +0300

    Fixed a bug due to which the combat mode not-terminated when there are no hostile critters.

--- sfall/Modules/BugFixes.cpp
+++ sfall/Modules/BugFixes.cpp
@@ -2215,6 +2215,20 @@ largeLoc:
 	}
 }
 
+static const DWORD combat_should_end_break = 0x422D00;
+static void __declspec(naked) combat_should_end_hack() {
+	__asm { // ecx = dude.team_num
+		cmp  ecx, [ebp + 0x50]; // npc who_hit_me.team_num
+		je   break;
+		test byte ptr [edx], 1; // npc combat_data.combat_state
+		jnz  break;
+		retn; // check next critter
+break:
+		add  esp, 4;
+		jmp  combat_should_end_break;
+	}
+}
+
 void BugFixes::init()
 {
 	#ifndef NDEBUG
@@ -2802,6 +2816,10 @@ void BugFixes::init()
 	SafeWrite8(0x4C46AB, 0x58); // esi >> ebx
 	// Fix the position of the target marker for small/medium location circles
 	MakeCall(0x4C03AA, wmWorldMap_hack, 2);
+
+	// Fix for the automatic termination combat mode when there are no hostile critters
+	MakeCall(0x422CF3, combat_should_end_hack);
+	SafeWrite16(0x422CEA, 0x0C74); // jz 0x422CF8
 }
 
 }
