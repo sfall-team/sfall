199502782ba0fb5d164944b356a30dcc4c944c7f
NovaRain <novarain@gmail.com>
2019-11-19 12:41:56 +0800

    Fixed a crash bug in CorpseLineOfFireFix (from commit 521b6c7, #270)
    Updated version number for hotfix.

--- sfall/version.h
+++ sfall/version.h
@@ -25,6 +25,6 @@
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 2
 #define VERSION_BUILD 1
-#define VERSION_REV 0
+#define VERSION_REV 1
 
-#define VERSION_STRING "4.2.1"
+#define VERSION_STRING "4.2.1.1"
