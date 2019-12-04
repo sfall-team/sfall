80cf1a9ca7823d8c86c3cee04c0524e132785b77
NovaRain <novarain@gmail.com>
2019-12-04 12:57:16 +0800

    Fix for printing game messages to debug log

--- sfall/FalloutEngine/VariableOffsets.h
+++ sfall/FalloutEngine/VariableOffsets.h
@@ -46,6 +46,7 @@
 #define FO_VAR_curr_stack                 0x59E96C
 #define FO_VAR_currentWindow              0x51DCB8
 #define FO_VAR_cursor_line                0x664514
+#define FO_VAR_debug_func                 0x51DF04
 #define FO_VAR_dialogue_head              0x518850
 #define FO_VAR_dialogue_state             0x518714
 #define FO_VAR_dialogue_switch_mode       0x518718
