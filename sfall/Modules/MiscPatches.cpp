c7ccb7f2c2f7bf3c74ef0a34b12446108381b961
NovaRain <novarain@gmail.com>
2020-05-08 10:41:56 +0800

    Edited the msg for SkipLoadingGameSettings a bit
    Added a few more defines.

--- sfall/Modules/MiscPatches.cpp
+++ sfall/Modules/MiscPatches.cpp
@@ -560,16 +560,18 @@ static void PartyMemberSkillPatch() {
 }
 
 static void SkipLoadingGameSettingsPatch() {
-	int skipLoading = GetConfigInt("Misc", "SkipLoadingGameSettings", 0);
-	if (skipLoading) {
-		dlog("Applying skip loading game settings from a saved game patch.", DL_INIT);
+	if (int skipLoading = GetConfigInt("Misc", "SkipLoadingGameSettings", 0)) {
+		dlog("Applying skip loading game settings from saved games patch.", DL_INIT);
 		BlockCall(0x493421);
 		SafeWrite8(0x4935A8, 0x1F);
 		SafeWrite32(0x4935AB, 0x90901B75);
 		CodeData PatchData;
 		if (skipLoading == 2) SafeWriteBatch<CodeData>(PatchData, {0x49341C, 0x49343B});
-		SafeWriteBatch<CodeData>(PatchData, {0x493450, 0x493465, 0x49347A, 0x49348F, 0x4934A4, 0x4934B9, 0x4934CE, 0x4934E3,
-		                                     0x4934F8, 0x49350D, 0x493522, 0x493547, 0x493558, 0x493569, 0x49357A});
+		SafeWriteBatch<CodeData>(PatchData, {
+			0x493450, 0x493465, 0x49347A, 0x49348F, 0x4934A4, 0x4934B9, 0x4934CE,
+			0x4934E3, 0x4934F8, 0x49350D, 0x493522, 0x493547, 0x493558, 0x493569,
+			0x49357A
+		});
 		dlogr(" Done", DL_INIT);
 	}
 }
