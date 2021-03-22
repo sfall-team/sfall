sfall-team/sfall

commit d95e43956d69a102c1965baa58af9f97626ff2f1

Fixed set_critter_burst_disable script function
* now applies only to weapons with burst attack as the secondary mode.

Minor edits to code comments.

sfall/Modules/Combat.cpp
@@ -27,7 +27,7 @@
 
 #include "HookScripts\CombatHS.h"
 
-#include "..\Game\items.h"
+//#include "..\Game\items.h"
 
 #include "SubModules\CombatBlock.h"
 
@@ -39,7 +39,7 @@ namespace sfall
 static const DWORD bodypartAddr[] = {
 	0x425562,                     // combat_display_
 	0x42A68F, 0x42A739,           // ai_called_shot_
-//	0x429E82, 0x429EC2, 0x429EFF, // ai_pick_hit_mode_
+//	0x429E82, 0x429EC2, 0x429EFF, // ai_pick_hit_mode_ (used for hit mode)
 	0x423231, 0x423268,           // check_ranged_miss_
 	0x4242D4,                     // attack_crit_failure_
 	// for combat_ctd_init_ func
@@ -56,7 +56,7 @@ static const DWORD bodypartAddr[] = {
 	0x49C00C,                     // protinstTestDroppedExplosive_
 };
 
-static struct BodyParts {
+static struct {
 	long Head;
 	long Left_Arm;
 	long Right_Arm;
@@ -66,7 +66,7 @@ static struct BodyParts {
 	long Eyes;
 	long Groin;
 	long Uncalled;
-} bodypartHit;
+} bodyPartHit;
 
 struct KnockbackModifier {
 	long id;
@@ -76,7 +76,7 @@ struct KnockbackModifier {
 
 long Combat::determineHitChance; // the value of hit chance w/o any cap
 
-static std::vector<long> noBursts; // object id
+static std::vector<long> noBursts; // critter id
 
 static std::vector<KnockbackModifier> mTargets;
 static std::vector<KnockbackModifier> mAttackers;
@@ -263,26 +263,34 @@ static void __declspec(naked) determine_to_hit_func_hack() {
 	}
 }
 
-static long __fastcall CheckDisableBurst(fo::GameObject* critter) {
+bool Combat::IsBurstDisabled(fo::GameObject* critter) {
 	for (size_t i = 0; i < noBursts.size(); i++) {
-		if (noBursts[i] == critter->id) {
-			return 10; // Disable Burst (area_attack_mode - non-existent value)
-		}
+		if (noBursts[i] == critter->id) return true;
+	}
+	return false;
+}
+
+static long __fastcall CheckDisableBurst(fo::GameObject* critter, fo::GameObject* weapon) {
+	if (fo::func::item_w_anim_weap(weapon, fo::AttackType::ATKTYPE_RWEAPON_SECONDARY) == fo::Animation::ANIM_fire_burst &&
+		Combat::IsBurstDisabled(critter))
+	{
+		return 10; // Disable Burst (area_attack_mode - non-existent value)
 	}
 	return 0;
 }
 
-static void __declspec(naked) ai_pick_hit_mode_hack_noSecondary() {
+static void __declspec(naked) ai_pick_hit_mode_hack_noBurst() {
 	__asm {
 		mov  ebx, [eax + 0x94]; // cap->area_attack_mode
-		push eax;
+		//push eax;
 		push ecx;
-		mov  ecx, esi;          // source
+		mov  edx, ebp; // weapon
+		mov  ecx, esi; // source
 		call CheckDisableBurst;
 		test eax, eax;
 		cmovnz ebx, eax;
 		pop  ecx;
-		pop  eax;
+		//pop  eax;
 		retn;
 	}
 }
@@ -388,7 +396,7 @@ static void __declspec(naked) item_w_called_shot_hook() {
 	static const DWORD aimedShotRet2 = 0x478EEA;
 	__asm {
 		push edx;
-		mov  ecx, edx;       // item
+		mov  ecx, edx; // item
 		call AimedShotTest;
 		test eax, eax;
 		jg   force;
@@ -434,35 +442,38 @@ void __stdcall ForceAimedShots(DWORD pid) {
 
 static void BodypartHitChances() {
 	using fo::var::hit_location_penalty;
-	hit_location_penalty[0] = bodypartHit.Head;
-	hit_location_penalty[1] = bodypartHit.Left_Arm;
-	hit_location_penalty[2] = bodypartHit.Right_Arm;
-	hit_location_penalty[3] = bodypartHit.Torso;
-	hit_location_penalty[4] = bodypartHit.Right_Leg;
-	hit_location_penalty[5] = bodypartHit.Left_Leg;
-	hit_location_penalty[6] = bodypartHit.Eyes;
-	hit_location_penalty[7] = bodypartHit.Groin;
-	hit_location_penalty[8] = bodypartHit.Uncalled;
+	hit_location_penalty[0] = bodyPartHit.Head;
+	hit_location_penalty[1] = bodyPartHit.Left_Arm;
+	hit_location_penalty[2] = bodyPartHit.Right_Arm;
+	hit_location_penalty[3] = bodyPartHit.Torso;
+	hit_location_penalty[4] = bodyPartHit.Right_Leg;
+	hit_location_penalty[5] = bodyPartHit.Left_Leg;
+	hit_location_penalty[6] = bodyPartHit.Eyes;
+	hit_location_penalty[7] = bodyPartHit.Groin;
+	hit_location_penalty[8] = bodyPartHit.Uncalled;
 }
 
 static void BodypartHitReadConfig() {
-	bodypartHit.Head      = static_cast<long>(GetConfigInt("Misc", "BodyHit_Head", -40));
-	bodypartHit.Left_Arm  = static_cast<long>(GetConfigInt("Misc", "BodyHit_Left_Arm", -30));
-	bodypartHit.Right_Arm = static_cast<long>(GetConfigInt("Misc", "BodyHit_Right_Arm", -30));
-	bodypartHit.Torso     = static_cast<long>(GetConfigInt("Misc", "BodyHit_Torso", 0));
-	bodypartHit.Right_Leg = static_cast<long>(GetConfigInt("Misc", "BodyHit_Right_Leg", -20));
-	bodypartHit.Left_Leg  = static_cast<long>(GetConfigInt("Misc", "BodyHit_Left_Leg", -20));
-	bodypartHit.Eyes      = static_cast<long>(GetConfigInt("Misc", "BodyHit_Eyes", -60));
-	bodypartHit.Groin     = static_cast<long>(GetConfigInt("Misc", "BodyHit_Groin", -30));
-	bodypartHit.Uncalled  = static_cast<long>(GetConfigInt("Misc", "BodyHit_Torso_Uncalled", 0));
+	bodyPartHit.Head      = static_cast<long>(GetConfigInt("Misc", "BodyHit_Head",          -40));
+	bodyPartHit.Left_Arm  = static_cast<long>(GetConfigInt("Misc", "BodyHit_Left_Arm",      -30));
+	bodyPartHit.Right_Arm = static_cast<long>(GetConfigInt("Misc", "BodyHit_Right_Arm",     -30));
+	bodyPartHit.Torso     = static_cast<long>(GetConfigInt("Misc", "BodyHit_Torso",           0));
+	bodyPartHit.Right_Leg = static_cast<long>(GetConfigInt("Misc", "BodyHit_Right_Leg",     -20));
+	bodyPartHit.Left_Leg  = static_cast<long>(GetConfigInt("Misc", "BodyHit_Left_Leg",      -20));
+	bodyPartHit.Eyes      = static_cast<long>(GetConfigInt("Misc", "BodyHit_Eyes",          -60));
+	bodyPartHit.Groin     = static_cast<long>(GetConfigInt("Misc", "BodyHit_Groin",         -30));
+	bodyPartHit.Uncalled  = static_cast<long>(GetConfigInt("Misc", "BodyHit_Torso_Uncalled",  0));
 }
 
 static void __declspec(naked)  ai_pick_hit_mode_hook_bodypart() {
+	using fo::Uncalled;
 	__asm {
-		mov  ebx, 8; // replace Body_Torso with Body_Uncalled
+		mov  ebx, Uncalled; // replace Body_Torso with Body_Uncalled
 		jmp  fo::funcoffs::determine_to_hit_;
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 static void __declspec(naked) apply_damage_hack() {
 	__asm {
@@ -476,8 +487,8 @@ end:
 	}
 }
 
-static void CombatProcFix() {
-	//Ray's combat_p_proc fix
+static void CombatProcPatch() {
+	// Ray's combat_p_proc fix
 	dlog("Applying Ray's combat_p_proc patch.", DL_INIT);
 	MakeCall(0x424DD9, apply_damage_hack);
 	SafeWrite16(0x424DC6, 0x9090);
@@ -497,7 +508,8 @@ static void Combat_OnGameLoad() {
 
 void Combat::init() {
 	CombatBlock::init();
-	CombatProcFix();
+
+	CombatProcPatch();
 
 	MakeCall(0x424B76, compute_damage_hack, 2);     // KnockbackMod
 	MakeJump(0x4136D3, compute_dmg_damage_hack);    // for op_critter_dmg
@@ -505,8 +517,8 @@ void Combat::init() {
 	MakeCall(0x424791, determine_to_hit_func_hack); // HitChanceMod
 	BlockCall(0x424796);
 
-	// Actually disables all secondary attacks for the critter, regardless of whether the weapon has a burst attack
-	MakeCall(0x429E44, ai_pick_hit_mode_hack_noSecondary, 1);   // NoBurst
+	// Disables secondary burst attacks for the critter
+	MakeCall(0x429E44, ai_pick_hit_mode_hack_noBurst, 1);
 
 	if (GetConfigInt("Misc", "CheckWeaponAmmoCost", 0)) {
 		MakeCall(0x4234B3, compute_spray_hack, 1);

sfall/FalloutEngine/Enums.h
@@ -129,6 +129,19 @@ enum CritterFlags : long
 	NoKnockBack  = 0x4000, // Can't be knocked back
 };
 
+enum ItemFlags : long
+{
+	// Weapon Flags:
+	BigGun       = 0x00000100,
+	TwoHand      = 0x00000200,
+	EnergyGun    = 0x00000400, // sfall flag
+
+	// Action Flags:
+	Use          = 0x00000800, // object can be used
+
+	HiddenItem   = 0x08000000  // item is hidden
+};
+
 enum DamageFlag : unsigned long
 {
 	DAM_KNOCKED_OUT     = 0x1,
@@ -217,25 +230,25 @@ enum class Material : long
 
 namespace ObjectFlag {
 	enum ObjectFlag : unsigned long {
-		Mouse_3d     = 0x1,
-		WalkThru     = 0x4,
-		Flat         = 0x8,
-		NoBlock      = 0x10,
-		Lighting     = 0x20,
-		Temp         = 0x400,
-		MultiHex     = 0x800,
-		NoHighlight  = 0x1000,
-		Used         = 0x2000,
-		TransRed     = 0x4000,
-		TransNone    = 0x8000,
-		TransWall    = 0x10000,
-		TransGlass   = 0x20000,
-		TransSteam   = 0x40000,
-		TransEnergy  = 0x80000,
-		Left_Hand    = 0x1000000,
-		Right_Hand   = 0x2000000,
-		Worn         = 0x4000000,
-		HiddenItem   = 0x8000000,
+		Mouse_3d     = 0x00000001,
+		WalkThru     = 0x00000004,
+		Flat         = 0x00000008,
+		NoBlock      = 0x00000010,
+		Lighting     = 0x00000020,
+		Temp         = 0x00000400,
+		MultiHex     = 0x00000800,
+		NoHighlight  = 0x00001000,
+		Used         = 0x00002000,
+		TransRed     = 0x00004000,
+		TransNone    = 0x00008000,
+		TransWall    = 0x00010000,
+		TransGlass   = 0x00020000,
+		TransSteam   = 0x00040000,
+		TransEnergy  = 0x00080000,
+		Left_Hand    = 0x01000000,
+		Right_Hand   = 0x02000000,
+		Worn         = 0x04000000,
+		HiddenItem   = 0x08000000,
 		WallTransEnd = 0x10000000,
 		LightThru    = 0x20000000,
 		Seen         = 0x40000000,
@@ -834,11 +847,11 @@ namespace WinFlags {
 namespace AIpref {
 	enum distance : long
 	{
-		stay_close            = 0, // the attacker will stay at a distance no more than 5 hexes from the player (defined in ai_move_steps_closer, cai_perform_distance_prefs)
+		stay_close            = 0, // the attacker will stay at a distance no more than 5 hexes from the player (behavior only for party members, defined in ai_move_steps_closer, cai_perform_distance_prefs)
 		charge                = 1, // AI will always try to get close to its target before or after attack
-		snipe                 = 2, // when the distance between the attacker and the target decreases, the attacker will try to move away from the target to a distance of up to 10 hexes
+		snipe                 = 2, // keep distance, when the distance between the attacker and the target decreases, the attacker will try to move away from the target to a distance of up to 10 hexes
 		on_your_own           = 3, // no special behavior defined for this
-		stay                  = 4  // the attacker will, if possible, stay at the hex where the combat started (defined in ai_move_steps_closer, ai_move_away)
+		stay                  = 4  // the attacker will, if possible, stay at the hex where the combat started (behavior defined in ai_move_steps_closer, ai_move_away)
 	};
 
 	// presets for party members

sfall/FalloutEngine/Functions_def.h
@@ -49,6 +49,7 @@ WRAP_WATCOM_FFUNC4(long, queue_add, long, time, fo::GameObject*, object, void*, 
 WRAP_WATCOM_FFUNC4(void, register_object_call, long*, target, long*, source, void*, func, long, delay)
 WRAP_WATCOM_FFUNC4(long, register_object_move_to_object, fo::GameObject*, source, fo::GameObject*, target, long, distance, long, delay)
 WRAP_WATCOM_FFUNC4(long, register_object_run_to_object, fo::GameObject*, source, fo::GameObject*, target, long, distance, long, delay)
+WRAP_WATCOM_FFUNC3(long, register_object_play_sfx, fo::GameObject*, object, const char*, sfxName, long, delay)
 WRAP_WATCOM_FFUNC3(long, scr_get_local_var, long, sid, long, varId, long*, value)
 WRAP_WATCOM_FFUNC3(long, scr_set_local_var, long, sid, long, varId, long, value)
 WRAP_WATCOM_FFUNC6(long, text_object_create, fo::GameObject*, object, const char*, text, long, font, long, colorText, long, colorOutline, fo::BoundRect*, rect)
@@ -181,7 +182,7 @@ WRAP_WATCOM_FUNC1(const char*, map_get_short_name, long, mapID)
 WRAP_WATCOM_FUNC2(void, MapDirErase, const char*, folder, const char*, ext)
 WRAP_WATCOM_FUNC1(void, mem_free, void*, mem)
 WRAP_WATCOM_FUNC2(void*, mem_realloc, void*, lpmem, DWORD, msize)
-WRAP_WATCOM_FUNC2(long, message_add, fo::MessageList*, file, MessageNode*, msg)
+WRAP_WATCOM_FUNC2(long, message_add, fo::MessageList*, file, fo::MessageNode*, msg)
 WRAP_WATCOM_FUNC1(long, message_exit, fo::MessageList*, msgList) // Destroys message list
 WRAP_WATCOM_FUNC1(long, message_filter, fo::MessageList*, file)
 WRAP_WATCOM_FUNC2(long, message_load, fo::MessageList*, msgList, const char*, msgFilePath) // Loads MSG file into given MessageList
@@ -193,10 +194,10 @@ WRAP_WATCOM_FUNC0(void, mouse_hide)
 // Calculates path and returns it's length
 WRAP_WATCOM_FUNC6(long, make_path_func, fo::GameObject*, objectFrom, long, tileFrom, long, tileTo, char*, pathDataBuffer, long, arg5, void*, blockingFunc)
 WRAP_WATCOM_FUNC0(long, new_obj_id)
-WRAP_WATCOM_FUNC2(void, obj_bound, fo::GameObject*, object, BoundRect*, boundRect) // Calculates bounding box (rectangle) for a given object
+WRAP_WATCOM_FUNC2(void, obj_bound, fo::GameObject*, object, fo::BoundRect*, boundRect) // Calculates bounding box (rectangle) for a given object
 WRAP_WATCOM_FUNC1(long, obj_destroy, fo::GameObject*, object)
 WRAP_WATCOM_FUNC2(long, obj_dist, fo::GameObject*, obj_src, fo::GameObject*, obj_trg)
-WRAP_WATCOM_FUNC2(long, obj_erase_object, fo::GameObject*, object, BoundRect*, boundRect)
+WRAP_WATCOM_FUNC2(long, obj_erase_object, fo::GameObject*, object, fo::BoundRect*, boundRect)
 WRAP_WATCOM_FUNC0(fo::GameObject*, obj_find_first)
 WRAP_WATCOM_FUNC0(fo::GameObject*, obj_find_next)
 WRAP_WATCOM_FUNC2(fo::GameObject*, obj_find_first_at_tile, long, elevation, long, tileNum)
@@ -238,7 +239,6 @@ WRAP_WATCOM_FUNC3(long, register_object_light, fo::GameObject*, object, long, li
 // WRAP_WATCOM_FUNC3(long, register_object_must_call_, fo::GameObject*, object;
 WRAP_WATCOM_FUNC1(long, register_object_must_erase, fo::GameObject*, object)
 // WRAP_WATCOM_FUNC3(long, register_object_outline_, fo::GameObject*, object;
-// WRAP_WATCOM_FUNC3(long, register_object_play_sfx_, fo::GameObject*, object;
 WRAP_WATCOM_FUNC3(long, register_object_take_out, fo::GameObject*, object, long, holdFrameId, long, nothing)
 WRAP_WATCOM_FUNC3(long, register_object_turn_towards, fo::GameObject*, object, long, tileNum, long, nothing)
 WRAP_WATCOM_FUNC2(long, roll_random, long, minValue, long, maxValue)

sfall/Modules/AI.cpp
@@ -231,7 +231,7 @@ static long __fastcall AICheckBeforeWeaponSwitch(fo::GameObject* target, long &h
 	}
 
 	fo::GameObject* item = fo::func::ai_search_inven_weap(source, 1, target); // search based on AP
-	if (!item) return 1; // no weapon in inventory, continue searching for weapons on the map (call ai_switch_weapons_)
+	if (!item) return 1; // no weapon in inventory, continue to search weapons on the map (call ai_switch_weapons_)
 
 	// is using a close range weapon?
 	long wType = fo::func::item_w_subtype(item, fo::AttackType::ATKTYPE_RWEAPON_PRIMARY);
@@ -244,7 +244,7 @@ static long __fastcall AICheckBeforeWeaponSwitch(fo::GameObject* target, long &h
 static void __declspec(naked) ai_try_attack_hook_switch_weapon() {
 	__asm {
 		push edx;
-		push [ebx]; // weapon (push dword ptr [esp + 0x364 - 0x3C + 8];)
+		push [ebx]; // weapon
 		push esi;   // source
 		call AICheckBeforeWeaponSwitch; // ecx - target, edx - hit mode
 		pop  edx;
@@ -392,7 +392,6 @@ static long __fastcall CheckWeaponRangeAndApCost(fo::GameObject* source, fo::Gam
 	long targetDist  = fo::func::obj_dist(source, target);
 	if (targetDist > weaponRange) return 0; // don't use secondary mode
 
-	//return (source->critter.movePoints >= fo::func::item_mp_cost(source, fo::ATKTYPE_RWEAPON_SECONDARY, 0));
 	return (source->critter.movePoints >= game::Items::item_w_mp_cost(source, fo::ATKTYPE_RWEAPON_SECONDARY, 0)); // 1 - allow secondary mode
 }
 
@@ -547,13 +546,13 @@ void AI::init() {
 	if (iniGetInt("Debugging", "AIBugFixes", 1, ::sfall::ddrawIni) == 0) return;
 	#endif
 
-	// Fix for NPCs not fully reloading a weapon if it has more ammo capacity than a box of ammo
+	// Fix for NPCs not fully reloading a weapon if it has an ammo capacity more than a box of ammo
 	HookCalls(item_w_reload_hook, {
 		0x42AF15,           // cai_attempt_w_reload_
 		0x42A970, 0x42AA56, // ai_try_attack_
 	});
 
-	// Fix incorrect AP check and cost for AI when reloading a weapon
+	// Fix for the incorrect check and AP cost when AI reloads a weapon
 	HookCall(0x42A955, ai_try_attack_hook_cost_reload);
 	MakeCall(0x42A9DE, ai_try_attack_hook_cost1);
 	MakeCall(0x42AABC, ai_try_attack_hook_cost2, 4);
@@ -567,7 +566,7 @@ void AI::init() {
 	// Adds a check/roll for friendly critters in the line of fire when AI uses burst attacks
 	HookCall(0x421666, combat_safety_invalidate_weapon_func_hook_check);
 
-	// Fix AI weapon switching when not having enough action points
+	// Fix AI weapon switching when not having enough AP to make an attack
 	// AI will try to change attack mode before deciding to switch weapon
 	HookCall(0x42AB57, ai_try_attack_hook_switch_weapon);
 

sfall/Modules/Combat.h
@@ -23,16 +23,6 @@
 namespace sfall
 {
 
-class Combat : public Module {
-public:
-	const char* name() { return "Combat"; }
-	void init();
-
-	static long determineHitChance;
-
-	static DWORD __fastcall check_item_ammo_cost(fo::GameObject* weapon, DWORD hitMode);
-};
-
 struct ChanceModifier {
 	long id;
 	int maximum;
@@ -50,6 +40,18 @@ struct ChanceModifier {
 		maximum = 95;
 		mod = 0;
 	}
+};
+
+class Combat : public Module {
+public:
+	const char* name() { return "Combat"; }
+	void init();
+
+	static long determineHitChance;
+
+	static DWORD __fastcall check_item_ammo_cost(fo::GameObject* weapon, DWORD hitMode);
+
+	static bool IsBurstDisabled(fo::GameObject* critter);
 };
 
 void __stdcall SetHitChanceMax(fo::GameObject* critter, DWORD maximum, DWORD mod);

sfall/Modules/MiscPatches.cpp
@@ -652,7 +652,7 @@ static void DisableHorriganPatch() {
 }
 
 static void DisplaySecondWeaponRangePatch() {
-	// Display the range of the second attack mode in the inventory when you switch weapon modes in active item slots
+	// Display the range of the secondary attack mode in the inventory when you switch weapon modes in active item slots
 	//if (GetConfigInt("Misc", "DisplaySecondWeaponRange", 1)) {
 		dlog("Applying display second weapon range patch.", DL_INIT);
 		HookCall(0x472201, display_stats_hook);

sfall/Modules/Worldmap.cpp
@@ -16,6 +16,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
 #include <unordered_map>
 #include <math.h>
 
@@ -189,7 +190,7 @@ static void __declspec(naked) wmWorldMapFunc_hook() {
 }
 
 static void __declspec(naked) wmRndEncounterOccurred_hack() {
-	__asm {
+	__asm { // edx - _wmLastRndTime
 		xor  ecx, ecx;
 		cmp  edx, WorldMapEncounterRate;
 		retn;

commit 1cc978c0b15e681d1cb19841b31d0b88c824999a

Added a fix to the sorting of the quest list (#361)
Added a cosmetic to the dialog review interface.

Fixed a crash introduced in commit d95e439.

sfall/FalloutEngine/Structs.h
@@ -1094,6 +1094,14 @@ struct PartyMemberPerkListData {
 	long perkData[fo::Perk::PERK_count];
 };
 
+struct QuestData {
+	long location;
+	long description;
+	long gvarIndex;
+	long displayThreshold;
+	long completedThreshold;
+};
+
 #pragma pack(pop)
 
 }

sfall/Modules/BugFixes.cpp
@@ -2732,6 +2732,17 @@ skip1:
 	}
 }
 
+static void __declspec(naked) gdReviewDisplay_hack() {
+	__asm {
+		mov  ebx, eax;
+		mov  eax, -3;
+		cmp  ebx, 407; // vertical position limit
+		cmovge ecx, eax;
+		cmp  ecx, eax;
+		retn;
+	}
+}
+
 static void __declspec(naked) combat_ai_hook() {
 	__asm {
 		call fo::funcoffs::combat_should_end_;
@@ -3597,6 +3608,10 @@ void BugFixes::init()
 
 	// Fix for the player's money not being displayed in the dialog window after leaving the barter/combat control interface
 	HookCall(0x447ACD, gdialog_bk_hook);
+
+	// Cosmetic fix to the dialog review interface
+	// to prevent the player name from being displayed at the bottom of the window when the text is longer than one screen
+	MakeCall(0x445ECC, gdReviewDisplay_hack);
 
 	// Fix crash or animation glitch of the critter in combat when an explosion from explosives
 	// and the AI attack animation are performed simultaneously

sfall/Modules/Combat.cpp
@@ -288,7 +288,7 @@ static long __fastcall CheckDisableBurst(fo::GameObject* critter, fo::GameObject
 static void __declspec(naked) ai_pick_hit_mode_hack_noBurst() {
 	__asm {
 		mov  ebx, [eax + 0x94]; // cap->area_attack_mode
-		//push eax;
+		push eax;
 		push ecx;
 		mov  edx, ebp; // weapon
 		mov  ecx, esi; // source
@@ -296,7 +296,7 @@ static void __declspec(naked) ai_pick_hit_mode_hack_noBurst() {
 		test eax, eax;
 		cmovnz ebx, eax;
 		pop  ecx;
-		//pop  eax;
+		pop  eax;
 		retn;
 	}
 }

sfall/Modules/QuestList.cpp
@@ -178,7 +178,7 @@ static void AddPage(int lines) {
 }
 
 // Print quests page text
-static long __fastcall QuestsPrint(const char* text, int width, DWORD* buf, BYTE* count) {
+static long __fastcall QuestsPrint(BYTE* count, int width, DWORD* buf, const char* text) {
 	look_quests++; // quests counter
 
 	if (outRangeFlag) {
@@ -223,14 +223,11 @@ static void __declspec(naked) PipStatus_hack_print() {
 	static const DWORD PipStatus_BreakRet  = 0x4982A4;
 	__asm {
 		push ecx;
-		push ebx;
 		push edx;
-		push ecx;         // count
+		push eax;         // text
 		push ebx;         // buf
-		mov  ecx, eax;    // text
-		call QuestsPrint; // edx - width
+		call QuestsPrint; // ecx - count, edx - width
 		pop  edx;
-		pop  ebx;
 		pop  ecx;
 		cmp  eax, 0;
 		jl   jbreak;
@@ -302,7 +299,7 @@ skip:
 	}
 }
 
-static DWORD __fastcall ActionButtons(register DWORD key) {
+static DWORD __fastcall ActionButtons(DWORD key) {
 	buttonsPressed = false;
 	if (key == 0x300) { // up
 		if (curent_quest_page == 0) return -1;
@@ -456,6 +453,34 @@ skip:
 	}
 }
 
+static void __fastcall QuestListSort(fo::QuestData* questList, size_t numElements) {
+	if (numElements <= 1) return;
+
+	fo::QuestData* tmpList = new fo::QuestData[numElements];
+	std::memcpy(tmpList, questList, numElements * sizeof(fo::QuestData));
+
+	long locStart = 1500; // number from which the locations starting
+	for (size_t i = 0; i < numElements;) {
+		for (size_t j = 0; j < numElements; j++) {
+			if (tmpList[j].location && tmpList[j].location == locStart) {
+				// unsorted?
+				if (j != i) questList[i] = tmpList[j];
+				tmpList[j].location = 0;
+				if (++i >= numElements) break;
+			}
+		}
+		locStart++;
+	}
+	delete[] tmpList;
+}
+
+static void __declspec(naked) quest_init_hook() {
+	__asm {
+		mov  ecx, eax;      // questList (base)
+		jmp  QuestListSort; // edx - numElements
+	}
+}
+
 void QuestListPatch() {
 	MakeCall(0x4974E4, StartPipboy_hack);
 
@@ -491,6 +516,9 @@ void QuestList::init() {
 	} else {
 		HookCall(0x498186, PipStatus_hook_printfix); // fix "out of bounds" bug when printing a list of quests
 	}
+
+	// Replace the qsort_ unstable sorting function with a simple stable sorting algorithm
+	HookCall(0x49A7C2, quest_init_hook);
 }
 
 }