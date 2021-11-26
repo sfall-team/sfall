8e3e49b28134edf035fa8e6a9d5fefaa66c0f116
NovaRain <novarain@gmail.com>
2021-11-27 06:44:06 +0800

    Removed UseCommandLine from ddraw.ini (always enabled)
    Updated version number.

--- sfall/Modules/MiscPatches.cpp
+++ sfall/Modules/MiscPatches.cpp
@@ -863,9 +863,9 @@ static void EngineOptimizationPatches() {
 	};
 	SafeWriteBatch<BYTE>(16, lsGameDelayAddr); // 41 to 16 ms
 	// LoadGame_
-	SafeWrite8(0x47CF0D, 195 + 10); // jz +10
+	SafeWrite8(0x47CF0D, 195 + 10); // jz 0x47CFDE
 	// SaveGame_
-	SafeWrite8(0x47C135, 140 + 10); // jz +10
+	SafeWrite8(0x47C135, 140 + 10); // jz 0x47C1CF
 }
 
 void MiscPatches::init() {
