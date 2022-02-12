2d97d24a39b2275eff34074964db60ea90aeed4d
NovaRain <novarain@gmail.com>
2022-02-13 07:37:11 +0800

    Reverted the limit on hit chance (commit 71bc271)
    * reason: the text length is depend on the frm.

--- sfall/Modules/HookScripts/CombatHs.cpp
+++ sfall/Modules/HookScripts/CombatHs.cpp
@@ -36,12 +36,6 @@ static void __declspec(naked) ToHitHook() {
 	__asm {
 		cmp  cRet, 1;
 		cmovge ebx, rets[0];
-		mov  eax, 999; // max
-		cmp  ebx, eax;
-		cmovg ebx, eax;
-		mov  eax, -99; // min
-		cmp  ebx, eax;
-		cmovl ebx, eax;
 		call EndHook;
 		mov  eax, ebx;
 		retn 8;
