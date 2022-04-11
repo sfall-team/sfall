e5f7d29a03176d7744a542e36121cc54790f9fc8
NovaRain <novarain@gmail.com>
2022-04-11 09:56:35 +0800

    [HRP] Fixed movie subtitles not showing up when setting MOVIE_SIZE=1
    * happens with certain combinations of screen and movie aspect ratios.
    
    Minor edit to Credits.cpp.
    Updated version number.

--- sfall/version.h
+++ sfall/version.h
@@ -25,6 +25,6 @@
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 3
 #define VERSION_BUILD 4
-#define VERSION_REV 0
+#define VERSION_REV 1
 
-#define VERSION_STRING "4.3.4"
+#define VERSION_STRING "4.3.4.1"
