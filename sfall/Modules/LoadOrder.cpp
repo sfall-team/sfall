f413bb8b9a9a69052647cdbed14adc188ac8e491
NovaRain <novarain@gmail.com>
2023-06-13 12:12:07 +0800

    Backported the new mods_order.txt mechanism

--- sfall/Modules/LoadOrder.cpp
+++ sfall/Modules/LoadOrder.cpp
@@ -16,6 +16,9 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
+#include <fstream>
+
 #include "..\main.h"
 #include "..\FalloutEngine\Fallout2.h"
 #include "..\Utils.h"
@@ -253,17 +256,42 @@ static void __fastcall game_init_databases_hook1() {
 }
 */
 static bool NormalizePath(std::string &path) {
+	const char* whiteSpaces = " \t";
+	// Remove comments.
+	size_t pos = path.find_first_of(";#");
+	if (pos != std::string::npos) {
+		path.erase(pos);
+	}
+	// Skip paths with colons.
 	if (path.find(':') != std::string::npos) return false;
 
-	int pos = 0;
-	do { // replace all '/' char with '\'
-		pos = path.find('/', pos);
-		if (pos != std::string::npos) path[pos] = '\\';
-	} while (pos != std::string::npos);
+	// Normalize directory separators.
+	std::replace(path.begin(), path.end(), '/', '\\');
 
+	// Disallow paths going outside of root folder.
 	if (path.find(".\\") != std::string::npos || path.find("..\\") != std::string::npos) return false;
-	while (path.front() == '\\') path.erase(0, 1); // remove firsts '\'
-	return true;
+
+	// Trim whitespaces.
+	path.erase(0, path.find_first_not_of(whiteSpaces)); // trim left
+	path.erase(path.find_last_not_of(whiteSpaces) + 1); // trim right
+
+	// Remove leading slashes.
+	path.erase(0, path.find_first_not_of('\\'));
+	return !path.empty();
+}
+
+static bool FileOrFolderExists(const std::string& path) {
+	return GetFileAttributesA(path.c_str()) != INVALID_FILE_ATTRIBUTES;
+}
+
+static bool FileExists(const std::string& path) {
+	DWORD dwAttrib = GetFileAttributesA(path.c_str());
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES && !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+static bool FolderExists(const std::string& path) {
+	DWORD dwAttrib = GetFileAttributesA(path.c_str());
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
 
 // Patches placed at the back of the vector will have priority in the chain over the front(previous) patches
@@ -272,9 +300,62 @@ static void GetExtraPatches() {
 	for (int i = 0; i < 100; i++) {
 		_itoa(i, &patchFile[9], 10);
 		std::string patch = IniReader::GetConfigString("ExtraPatches", patchFile, "", MAX_PATH);
-		if (patch.empty() || !NormalizePath(patch) || GetFileAttributes(patch.c_str()) == INVALID_FILE_ATTRIBUTES) continue;
+		if (patch.empty() || !NormalizePath(patch) || !FileOrFolderExists(patch)) continue;
 		patchFiles.push_back(patch);
 	}
+	const std::string modsPath = ".\\mods\\";
+	const std::string loadOrderFilePath = modsPath + "mods_order.txt";
+
+	dlogr("Loading custom patches:", DL_MAIN);
+
+	// If the mods folder does not exist, create it.
+	if (!FolderExists(modsPath)) {
+		CreateDirectoryA(modsPath.c_str(), 0);
+	}
+	// If load order file does not exist, initialize it automatically with mods already in the mods folder.
+	if (!FileExists(loadOrderFilePath)) {
+		std::ofstream loadOrderFile(loadOrderFilePath, std::ios::out | std::ios::trunc);
+		if (loadOrderFile.is_open()) {
+			// Search all .dat files and folders in the mods folder.
+			std::vector<std::string> autoLoadedPatchFiles;
+			std::string pathMask(modsPath + "*.dat");
+			WIN32_FIND_DATA findData;
+			HANDLE hFind = FindFirstFile(pathMask.c_str(), &findData);
+			if (hFind != INVALID_HANDLE_VALUE) {
+				do {
+					std::string name(findData.cFileName);
+					if ((name.length() - name.find_last_of('.')) > 4) continue;
+
+					autoLoadedPatchFiles.push_back(name);
+				} while (FindNextFile(hFind, &findData));
+				FindClose(hFind);
+			}
+			// Sort the search result.
+			std::sort(autoLoadedPatchFiles.begin(), autoLoadedPatchFiles.end());
+			// Write found files into load order file.
+			for (std::vector<std::string>::iterator it = autoLoadedPatchFiles.begin(); it != autoLoadedPatchFiles.end(); ++it) {
+				loadOrderFile << *it << '\n';
+			}
+		} else {
+			dlog_f("Error creating load order file %s.\n", DL_MAIN, loadOrderFilePath.c_str() + 2);
+		}
+	}
+	// Add mods from load order file.
+	std::ifstream loadOrderFile(loadOrderFilePath, std::ios::in);
+	if (loadOrderFile.is_open()) {
+		std::string patch;
+		while (std::getline(loadOrderFile, patch)) {
+			if (patch.empty() || !NormalizePath(patch)) continue;
+			patch = modsPath + patch;
+			if (!FileOrFolderExists(patch)) continue;
+
+			dlog_f("> %s\n", DL_MAIN, patch.c_str() + 2);
+			patchFiles.push_back(patch);
+		}
+	} else {
+		dlog_f("Error opening %s for read: %d\n", DL_MAIN, loadOrderFilePath.c_str() + 2, GetLastError());
+	}
+
 	// Remove first duplicates
 	size_t size = patchFiles.size();
 	for (size_t i = 0; i < size; ++i) {
