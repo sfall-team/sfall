0a6a604abcb24d98c3293e278e89ad6de0f5545a
phobos2077 <phobos2077@gmail.com>
2023-06-16 15:10:25 +0200

    Add support for using 0b prefix in INI files to denote binary base (consistent with GetPrivateProfile*)

--- sfall/Config.cpp
+++ sfall/Config.cpp
@@ -160,7 +160,13 @@ bool Config::getInt(const char* sectionKey, const char* key, int& outValue, unsi
 
     char* end;
     errno = 0;
-    outValue = strtol(value->c_str(), &end, base); // see https://stackoverflow.com/a/6154614
+	const char* rawValue = value->c_str();
+	// Support 0b prefix to detect binary values (for compatibility with GetPrivateProfile* functions).
+	if ((base == 0 || base == 2) && value->size() > 2 && rawValue[1] == 'b' && rawValue[0] == '0') {
+		rawValue = &rawValue[2];
+		base = 2;
+	}
+    outValue = strtol(rawValue, &end, base); // see https://stackoverflow.com/a/6154614
     return true;
 }
 
