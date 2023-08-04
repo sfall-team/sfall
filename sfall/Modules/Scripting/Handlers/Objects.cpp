sfall-team/sfall

commit 438bc6f78fce51164dadee674eee9e8b609ea281

Code edits to the weapon switching fix in AI.cpp
Added more struct defines and corrected related code.

sfall/Modules/Scripting/Handlers/Objects.cpp
@@ -99,7 +99,7 @@ void op_get_npc_level(OpcodeContext& ctx) {
 
 	if (findPid || name[0] != 0) {
 		DWORD pid = 0;
-		DWORD* members = fo::var::partyMemberList;
+		auto members = fo::var::partyMemberList;
 		for (DWORD i = 0; i < fo::var::partyMemberCount; i++) {
 			if (!findPid) {
 				__asm {
@@ -109,22 +109,21 @@ void op_get_npc_level(OpcodeContext& ctx) {
 					mov  critterName, eax;
 				}
 				if (!_stricmp(name, critterName)) { // found npc
-					pid = ((fo::GameObject*)*members)->protoId;
+					pid = members[i].object->protoId;
 					break;
 				}
 			} else {
-				DWORD _pid = ((fo::GameObject*)*members)->protoId;
+				DWORD _pid = members[i].object->protoId;
 				if (findPid == _pid) {
 					pid = _pid;
 					break;
 				}
 			}
-			members += 4;
 		}
 		if (pid) {
-			DWORD* pids = fo::var::partyMemberPidList;
+			DWORD* pidList = fo::var::partyMemberPidList;
 			for (DWORD j = 0; j < fo::var::partyMemberMaxCount; j++) {
-				if (pids[j] == pid) {
+				if (pidList[j] == pid) {
 					level = fo::var::partyMemberLevelUpInfoList[j * 3];
 					break;
 				}
@@ -339,9 +338,8 @@ void op_get_party_members(OpcodeContext& ctx) {
 	auto includeHidden = ctx.arg(0).rawValue();
 	int actualCount = fo::var::partyMemberCount;
 	DWORD arrayId = CreateTempArray(0, 4);
-	auto partyMemberList = fo::var::partyMemberList;
 	for (int i = 0; i < actualCount; i++) {
-		auto obj = reinterpret_cast<fo::GameObject*>(partyMemberList[i * 4]);
+		fo::GameObject* obj = fo::var::partyMemberList[i].object;
 		if (includeHidden || (obj->IsCritter() && !fo::func::critter_is_dead(obj) && !(obj->flags & fo::ObjectFlag::Mouse_3d))) {
 			arrays[arrayId].push_back((long)obj);
 		}

sfall/FalloutEngine/Functions_def.h
@@ -41,6 +41,7 @@ WRAP_WATCOM_FFUNC3(long, message_find, DWORD*, msgFile, long, msgNumber, DWORD*,
 WRAP_WATCOM_FFUNC4(long, mouse_click_in, long, x, long, y, long, x_offs, long, y_offs)
 WRAP_WATCOM_FFUNC4(long, mouse_in, long, x, long, y, long, x_offs, long, y_offs)
 WRAP_WATCOM_FFUNC3(fo::GameObject*, obj_blocking_at, fo::GameObject*, object, long, tile, long, elevation)
+WRAP_WATCOM_FFUNC4(long, obj_dist_with_tile, fo::GameObject*, source, long, sourceTile, fo::GameObject*, target, long, targetTile)
 WRAP_WATCOM_FFUNC4(long, obj_move_to_tile, fo::GameObject*, object, long, tile, long, elevation, RECT*, rect)
 WRAP_WATCOM_FFUNC3(long, obj_new_sid_inst, fo::GameObject*, object, long, sType, long, scriptIndex)
 WRAP_WATCOM_FFUNC3(fo::GameObject*, object_under_mouse, long, crSwitch, long, inclDude, long, elevation)

sfall/FalloutEngine/Structs.h
@@ -1082,6 +1082,18 @@ struct AudioFile {
 	long  position;
 };
 
+// aka PartyMemberRecoveryList
+struct ObjectListData {
+	GameObject* object;
+	fo::ScriptInstance* script;
+	long* localVarData;
+	ObjectListData* nextSaveList; // _itemSaveListHead
+};
+
+struct PartyMemberPerkListData {
+	long perkData[fo::Perk::PERK_count];
+};
+
 #pragma pack(pop)
 
 }

sfall/FalloutEngine/Variables.cpp
@@ -26,27 +26,31 @@ namespace var
 {
 
 // defines reference to a variable (pointer is constant, but value can be changed)
-#define VAR_(name, type)	\
-	type &name = *reinterpret_cast<type*>(FO_VAR_##name);
+#define VAR_(name, type) \
+	type& name = *reinterpret_cast<type*>(FO_VAR_##name);
 
 // defines reference to a constant variable (value can't be changed from sfall)
-#define VARC(name, type)	\
+#define VARC(name, type) \
 	const type& name = *reinterpret_cast<type*>(FO_VAR_##name);
 
 // defines reference to static array
-#define VARA(name, type, size)	\
+#define VARA(name, type, size) \
 	ArrayWrapper<type, size> &name = *reinterpret_cast<ArrayWrapper<type, size>*>(FO_VAR_##name);
 
 // defines reference to static 2-dimensional array
-#define VAR2(name, type, size1, size2)	\
+#define VAR2(name, type, size1, size2) \
 	ArrayWrapper<ArrayWrapper<type, size2>, size1> &name = *reinterpret_cast<ArrayWrapper<ArrayWrapper<type, size2>, size1>*>(FO_VAR_##name);
 
 // defines reference to static 3-dimensional array
-#define VAR3(name, type, size1, size2, size3)	\
+#define VAR3(name, type, size1, size2, size3) \
 	ArrayWrapper<ArrayWrapper<ArrayWrapper<type, size3>, size2>, size1> &name = *reinterpret_cast<ArrayWrapper<ArrayWrapper<ArrayWrapper<type, size3>, size2>, size1>*>(FO_VAR_##name);
 
+// defines reference pointer to dynamic array
+#define VARD(name, type) \
+	type*& name = *reinterpret_cast<type**>(FO_VAR_##name);
+
 // defines const pointer to variable (useful for static arrays, when exact size is unknown)
-#define VARP(name, type)	\
+#define VARP(name, type) \
 	type* const name = reinterpret_cast<type*>(FO_VAR_##name);
 
 // X-Macros pattern

sfall/FalloutEngine/Variables.h
@@ -52,26 +52,30 @@ struct ArrayWrapper {
 };
 
 // defines reference to an engine variable
-#define VAR_(name, type)	\
+#define VAR_(name, type) \
 	extern type& name;
 
-#define VARC(name, type)	\
+#define VARC(name, type) \
 	extern const type& name;
 
 // defines reference to static array
-#define VARA(name, type, size)	\
+#define VARA(name, type, size) \
 	extern ArrayWrapper<type, size> &name;
 
 // defines reference to static 2-dimensional array
-#define VAR2(name, type, size1, size2)	\
+#define VAR2(name, type, size1, size2) \
 	extern ArrayWrapper<ArrayWrapper<type, size2>, size1> &name;
 
 // defines reference to static 3-dimensional array
-#define VAR3(name, type, size1, size2, size3)	\
+#define VAR3(name, type, size1, size2, size3) \
 	extern ArrayWrapper<ArrayWrapper<ArrayWrapper<type, size3>, size2>, size1> &name;
 
+// defines reference pointer to dynamic array
+#define VARD(name, type) \
+	extern type*& name;
+
 // defines const pointer to variable (useful for static arrays, when exact size is unknown)
-#define VARP(name, type)	\
+#define VARP(name, type) \
 	extern type* const name;
 
 // TODO: assign appropriate types (arrays, structs, strings, etc.) for all variables

sfall/FalloutEngine/Variables_def.h
@@ -23,7 +23,7 @@ VAR_(cmap,                       DWORD)
 VAR_(colorTable,                 DWORD)
 VAR_(combat_end_due_to_load,     DWORD)
 VAR_(combat_free_move,           DWORD)
-VAR_(combat_list,                DWORD)
+VARD(combat_list,                fo::GameObject*)
 VAR_(combat_state,               DWORD)
 VAR_(combat_turn_running,        DWORD)
 VAR_(combatNumTurns,             DWORD)
@@ -62,7 +62,7 @@ VAR_(folder_card_title,          DWORD)
 VAR_(folder_card_title2,         DWORD)
 VAR_(frame_time,                 DWORD)
 VAR_(free_perk,                  char)
-VAR_(game_global_vars,           long*)  // dynamic array of size == num_game_global_vars
+VARD(game_global_vars,           long)  // dynamic array of size == num_game_global_vars
 VAR_(game_user_wants_to_quit,    DWORD)
 VAR_(gcsd,                       fo::CombatGcsd*)
 VAR_(gdBarterMod,                DWORD)
@@ -138,7 +138,7 @@ VAR_(num_game_global_vars,       DWORD)
 VAR_(num_map_global_vars,        DWORD)
 VAR_(num_windows,                DWORD)
 VAR_(obj_dude,                   fo::GameObject*)
-VARA(objectTable,                ObjectTable*, 40000)
+VARA(objectTable,                fo::ObjectTable*, 40000)
 VAR_(objItemOutlineState,        DWORD)
 VAR_(optionRect,                 DWORD)
 VAR_(optionsButtonDown,          DWORD)
@@ -152,9 +152,9 @@ VAR_(outlined_object,            fo::GameObject*)
 VAR_(partyMemberAIOptions,       DWORD)
 VAR_(partyMemberCount,           DWORD)
 VAR_(partyMemberLevelUpInfoList, DWORD*)
-VAR_(partyMemberList,            DWORD*) // each struct - 4 integers, first integer - objPtr
+VAR_(partyMemberList,            fo::ObjectListData*) // dynamic array
 VAR_(partyMemberMaxCount,        DWORD)
-VAR_(partyMemberPidList,         DWORD*)
+VARD(partyMemberPidList,         DWORD) // dynamic array
 VAR_(patches,                    char*)
 VAR_(paths,                      fo::PathNode*)  // array
 VAR2(pc_crit_succ_eff,           fo::CritInfo, 9, 6)  // 9 body parts, 6 effects
@@ -164,7 +164,7 @@ VAR_(pc_proto,                   fo::Proto)
 VARA(pc_trait,                   long, 2)  // 2 of them
 VAR_(PeanutButter,               BYTE)
 VARA(perk_data,                  fo::PerkInfo, fo::PERK_count)
-VAR_(perkLevelDataList,          long*) // dynamic array, limited to PERK_Count
+VARD(perkLevelDataList,          fo::PartyMemberPerkListData) // dynamic array, limited to (PERK_Count * partyMemberMaxCount)
 VARC(pip_win,                    DWORD)
 VAR_(pipboy_message_file,        fo::MessageList)
 VAR_(pipmesg,                    DWORD)
@@ -197,7 +197,7 @@ VAR_(square,                     DWORD)
 VAR_(squares,                    DWORD*)
 VARA(stack,                      DWORD, 10)
 VARA(stack_offset,               DWORD, 10)
-VARA(stat_data,                  fo::StatInfo, fo::STAT_real_max_stat) // dynamic array
+VARA(stat_data,                  fo::StatInfo, fo::STAT_real_max_stat)
 VAR_(stat_flag,                  DWORD)
 VAR_(subtitleList,               fo::SubTitleList*)
 VARA(sWindows,                   fo::sWindow, 16)
@@ -250,3 +250,4 @@ VAR_(YellowColor,                BYTE)
 #undef VARA
 #undef VAR2
 #undef VAR3
+#undef VARD

sfall/Modules/AI.cpp
@@ -220,33 +220,39 @@ isNotDead:
 	}
 }
 
-static long __fastcall AICheckBeforeWeaponSwitch(fo::GameObject* target, long &hitMode, fo::GameObject* source, fo::GameObject* weapon) {
+////////////////////////////////////////////////////////////////////////////////
+
+static long __fastcall ai_try_attack_switch_fix(fo::GameObject* target, long &hitMode, fo::GameObject* source, fo::GameObject* weapon) {
 	if (source->critter.movePoints <= 0) return -1; // exit from ai_try_attack_
-	if (!weapon) return 1; // no weapon in hand slot, call ai_switch_weapons_
-
-	long _hitMode = fo::func::ai_pick_hit_mode(source, weapon, target);
-	if (_hitMode != hitMode) {
-		hitMode = _hitMode;
-		return 0; // change hit mode, continue attack cycle
+	if (weapon) {
+		long _hitMode = fo::func::ai_pick_hit_mode(source, weapon, target);
+		if (_hitMode != hitMode) {
+			hitMode = _hitMode;
+			return 0; // change hit mode, continue attack cycle
+		}
 	}
 
+	// does the NPC have other weapons in inventory?
 	fo::GameObject* item = fo::func::ai_search_inven_weap(source, 1, target); // search based on AP
-	if (!item) return 1; // no weapon in inventory, continue to search weapons on the map (call ai_switch_weapons_)
-
-	// is using a close range weapon?
-	long wType = fo::func::item_w_subtype(item, fo::AttackType::ATKTYPE_RWEAPON_PRIMARY);
-	if (wType <= fo::AttackSubType::MELEE) { // unarmed and melee weapons, check the distance before switching
-		if (!AI::AttackInRange(source, item, fo::func::obj_dist(source, target))) return -1; // target out of range, exit ai_try_attack_
+	if (item && weapon) {
+		// is using a close range weapon?
+		long wType = fo::func::item_w_subtype(item, fo::AttackType::ATKTYPE_RWEAPON_PRIMARY);
+		if (wType <= fo::AttackSubType::MELEE) { // unarmed and melee weapons, check the distance before switching
+			if (!AI::AttackInRange(source, item, fo::func::obj_dist(source, target))) return -1; // target out of range, exit ai_try_attack_
+		}
+		return 1; // all good, execute vanilla behavior of ai_switch_weapons_ function
 	}
-	return 1; // execute vanilla behavior of ai_switch_weapons_ function
+	// no other weapon in inventory
+	return (weapon) ? -1 // exit, NPC has a weapon in hand slot, so we don't look for another weapon on the map
+	                : 1; // no weapon in inventory or hand slot, continue to search weapons on the map (call ai_switch_weapons_)
 }
 
-static void __declspec(naked) ai_try_attack_hook_switch_weapon() {
+static void __declspec(naked) ai_try_attack_hook_switch_fix() {
 	__asm {
 		push edx;
 		push [ebx]; // weapon
 		push esi;   // source
-		call AICheckBeforeWeaponSwitch; // ecx - target, edx - hit mode
+		call ai_try_attack_switch_fix; // ecx - target, edx - hit mode
 		pop  edx;
 		test eax, eax;
 		jle  noSwitch; // <= 0
@@ -257,6 +263,8 @@ noSwitch:
 		retn; // -1 - for exit from ai_try_attack_
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 static long RetryCombatMinAP;
 
@@ -543,6 +551,7 @@ void AI::init() {
 		dlogr(" Done", DL_INIT);
 	}
 
+	/////////////////////// Combat behavior AI fixes ///////////////////////
 	#ifndef NDEBUG
 	if (IniReader::GetIntDefaultConfig("Debugging", "AIBugFixes", 1) == 0) return;
 	#endif
@@ -561,15 +570,13 @@ void AI::init() {
 	// Adds a check for the weapon range and the AP cost when AI is choosing weapon attack modes
 	HookCall(0x429F6D, ai_pick_hit_mode_hook);
 
-	/////////////////////// Combat AI behavior fixes ///////////////////////
+	// Fix AI weapon switching when not having enough AP to make an attack
+	// AI will try to change attack mode before deciding to switch weapon
+	HookCall(0x42AB57, ai_try_attack_hook_switch_fix);
 
 	// Fix to reduce friendly fire in burst attacks
 	// Adds a check/roll for friendly critters in the line of fire when AI uses burst attacks
 	HookCall(0x421666, combat_safety_invalidate_weapon_func_hook_check);
-
-	// Fix AI weapon switching when not having enough AP to make an attack
-	// AI will try to change attack mode before deciding to switch weapon
-	HookCall(0x42AB57, ai_try_attack_hook_switch_weapon);
 
 	// Fix for duplicate critters being added to the list of potential targets for AI
 	MakeCall(0x428E75, ai_find_attackers_hack_target2, 2);

sfall/Modules/PartyControl.cpp
@@ -129,9 +129,9 @@ static void SaveRealDudeState() {
 	realDude.obj_dude = fo::var::obj_dude;
 	realDude.art_vault_guy_num = fo::var::art_vault_guy_num;
 	realDude.itemCurrentItem = fo::var::itemCurrentItem;
-	memcpy(realDude.itemButtonItems, fo::var::itemButtonItems, sizeof(fo::ItemButtonItem) * 2);
-	memcpy(realDude.traits, fo::var::pc_trait, sizeof(long) * 2);
-	memcpy(realDude.perkLevelDataList, fo::var::perkLevelDataList, sizeof(DWORD) * fo::PERK_count);
+	std::memcpy(realDude.itemButtonItems, fo::var::itemButtonItems, sizeof(fo::ItemButtonItem) * 2);
+	std::memcpy(realDude.traits, fo::var::pc_trait, sizeof(long) * 2);
+	std::memcpy(realDude.perkLevelDataList, fo::var::perkLevelDataList, sizeof(DWORD) * fo::PERK_count);
 	strcpy_s(realDude.pc_name, sizeof(fo::var::pc_name), fo::var::pc_name);
 	realDude.Level = fo::var::Level_;
 	realDude.last_level = fo::var::last_level;
@@ -177,11 +177,9 @@ static void SetCurrentDude(fo::GameObject* npc) {
 	// copy existing party member perks or reset list for non-party member NPC
 	long isPartyMember = fo::IsPartyMemberByPid(npc->protoId);
 	if (isPartyMember) {
-		memcpy(fo::var::perkLevelDataList, fo::var::perkLevelDataList + (fo::PERK_count * (isPartyMember - 1)), sizeof(DWORD) * fo::PERK_count);
+		std::memcpy(fo::var::perkLevelDataList, fo::var::perkLevelDataList[isPartyMember - 1].perkData, sizeof(DWORD) * fo::Perk::PERK_count);
 	} else {
-		for (int i = 0; i < fo::PERK_count; i++) {
-			fo::var::perkLevelDataList[i] = 0;
-		}
+		std::memset(fo::var::perkLevelDataList, 0, sizeof(DWORD) * fo::Perk::PERK_count);
 	}
 
 	// change level
@@ -216,7 +214,7 @@ static void SetCurrentDude(fo::GameObject* npc) {
 			if (weaponState[i].leftIsCopy) {
 				auto item = fo::func::inven_left_hand(npc);
 				if (item && item->protoId == weaponState[i].leftSlot.item->protoId) {
-					memcpy(&fo::var::itemButtonItems[0], &weaponState[i].leftSlot, 0x14);
+					std::memcpy(&fo::var::itemButtonItems[0], &weaponState[i].leftSlot, 0x14);
 					isMatch = true;
 				}
 				weaponState[i].leftIsCopy = false;
@@ -224,7 +222,7 @@ static void SetCurrentDude(fo::GameObject* npc) {
 			if (weaponState[i].rightIsCopy) {
 				auto item = fo::func::inven_right_hand(npc);
 				if (item && item->protoId == weaponState[i].rightSlot.item->protoId) {
-					memcpy(&fo::var::itemButtonItems[1], &weaponState[i].rightSlot, 0x14);
+					std::memcpy(&fo::var::itemButtonItems[1], &weaponState[i].rightSlot, 0x14);
 					isMatch = true;
 				}
 				weaponState[i].rightIsCopy = false;
@@ -292,9 +290,9 @@ static void RestoreRealDudeState(bool redraw = true) {
 	fo::var::art_vault_guy_num = realDude.art_vault_guy_num;
 
 	fo::var::itemCurrentItem = realDude.itemCurrentItem;
-	memcpy(fo::var::itemButtonItems, realDude.itemButtonItems, sizeof(fo::ItemButtonItem) * 2);
-	memcpy(fo::var::pc_trait, realDude.traits, sizeof(long) * 2);
-	memcpy(fo::var::perkLevelDataList, realDude.perkLevelDataList, sizeof(DWORD) * fo::PERK_count);
+	std::memcpy(fo::var::itemButtonItems, realDude.itemButtonItems, sizeof(fo::ItemButtonItem) * 2);
+	std::memcpy(fo::var::pc_trait, realDude.traits, sizeof(long) * 2);
+	std::memcpy(fo::var::perkLevelDataList, realDude.perkLevelDataList, sizeof(DWORD) * fo::PERK_count);
 	strcpy_s(fo::var::pc_name, sizeof(fo::var::pc_name), realDude.pc_name);
 	fo::var::Level_ = realDude.Level;
 	fo::var::last_level = realDude.last_level;
@@ -471,11 +469,11 @@ bool PartyControl::IsNpcControlled() {
 static bool CopyItemSlots(WeaponStateSlot &element, bool isSwap) {
 	bool isCopy = false;
 	if (fo::var::itemButtonItems[0 + isSwap].itsWeapon && fo::var::itemButtonItems[0 + isSwap].item) {
-		memcpy(&element.leftSlot, &fo::var::itemButtonItems[0 + isSwap], 0x14);
+		std::memcpy(&element.leftSlot, &fo::var::itemButtonItems[0 + isSwap], 0x14);
 		element.leftIsCopy = isCopy = true;
 	}
 	if (fo::var::itemButtonItems[1 - isSwap].itsWeapon && fo::var::itemButtonItems[1 - isSwap].item) {
-		memcpy(&element.rightSlot, &fo::var::itemButtonItems[1 - isSwap], 0x14);
+		std::memcpy(&element.rightSlot, &fo::var::itemButtonItems[1 - isSwap], 0x14);
 		element.rightIsCopy = isCopy = true;
 	}
 	if (isSwap && isCopy) {

commit 61f3a2ac13a1d255aea1c56c8d2a204903a065f6

Fixed the backward compatibility of get_npc_level
(broken in commit 438bc6f)

sfall/Modules/Scripting/Handlers/Objects.cpp
@@ -102,12 +102,7 @@ void op_get_npc_level(OpcodeContext& ctx) {
 		auto members = fo::var::partyMemberList;
 		for (DWORD i = 0; i < fo::var::partyMemberCount; i++) {
 			if (!findPid) {
-				__asm {
-					mov  eax, members;
-					mov  eax, [eax];
-					call fo::funcoffs::critter_name_;
-					mov  critterName, eax;
-				}
+				critterName = fo::func::critter_name(members[i].object);
 				if (!_stricmp(name, critterName)) { // found npc
 					pid = members[i].object->protoId;
 					break;