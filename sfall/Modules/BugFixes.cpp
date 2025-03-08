sfall-team/sfall

commit a34aa64cedacc1aa0a3c9478aa1eca89932a25d9

Added AutoSearchSFX option for automatic search of SFX files
Excluded ANIM_walk from the debug log about missing critter art files.

sfall/Modules/DebugEditor.cpp
@@ -313,9 +313,9 @@ hide:
 }
 
 static void __declspec(naked) art_data_size_hook() {
-	static const char* artDbgMsg = "\nERROR: File not found: %s\n";
+	static const char* artDbgMsg = "\nERROR: Art file not found: %s\n";
 	__asm {
-		test edi, edi;
+		test edi, edi; // 1 - isExistsArt
 		jz   artNotExist;
 		retn;
 artNotExist:
@@ -336,6 +336,18 @@ display:
 		add  esp, 20;
 		lea  eax, [esp + 4];
 		jmp  fo::funcoffs::display_print_;
+	}
+}
+
+static void __declspec(naked) art_data_size_hook_check() {
+	using namespace fo;
+	__asm {
+		xor  esi, esi;
+		mov  eax, ebx; // ebx - FID
+		shr  eax, 16;  // al - animation code (ID2)
+		cmp  al, ANIM_walk;
+		cmove ecx, esi;
+		retn;
 	}
 }
 
@@ -437,11 +449,16 @@ static void DebugModePatch() {
 		} else {
 			SafeWrite32(0x4C6D9C, (DWORD)debugGnw);
 		}
+
 		if (IniReader::GetIntDefaultConfig("Debugging", "HideObjIsNullMsg", 0)) {
 			MakeJump(0x453FD2, dbg_error_hack);
 		}
+
 		// prints a debug message about a missing critter art file to both debug.log and the message window in sfall debugging mode
 		HookCall(0x419B65, art_data_size_hook);
+		// checks the animation code, if ANIM_walk then skip printing the debug message
+		HookCall(0x419AA0, art_data_size_hook_check);
+		SafeWrite8(0x419B61, CodeType::JumpNZ); // jz > jnz
 
 		// Fix to prevent crashes when there is a '%' character in the printed message
 		if (dbgMode > 1) {

artifacts/ddraw.ini
@@ -1,5 +1,5 @@
 ;sfall configuration settings
-;v4.3
+;v4.3.1
 
 [Main]
 ;Change to 1 if you want to use command line args to tell sfall to use another ini file.
@@ -142,6 +142,11 @@ AllowDShowSound=0
 ;Set to 1 to override the default music path with data\sound\music\ if music_path is not present in the cfg
 ;Set to 2 to overwrite all occurrences of the music path
 OverrideMusicDir=1
+
+;Set to 1 to automatically search for new SFX sound files at game startup
+;Note: With this option enabled, you will no longer need to use the utility regsnd.exe to register new SFX sounds
+;This will also increase the loading time of the game
+AutoSearchSFX=1
 
 ;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 [Input]

sfall/Modules/Sound.cpp
@@ -938,6 +938,13 @@ skip:
 	}
 }
 
+static void __declspec(naked) sfxl_init_hook() {
+	__asm {
+		xor  eax, eax;
+		retn;
+	}
+}
+
 constexpr int SampleRate = 44100; // 44.1kHz
 
 void Sound::init() {
@@ -1013,6 +1020,10 @@ void Sound::init() {
 
 	// Support for ACM audio file playback and volume control for the soundplay script function
 	HookCall(0x4661B3, soundStartInterpret_hook);
+
+	if (IniReader::GetConfigInt("Sound", "AutoSearchSFX", 1)) {
+		HookCalls(sfxl_init_hook, {0x4A9999, 0x4A9B34});
+	}
 }
 
 void Sound::exit() {

commit 3a29db5f90d87ba526cd8a1964dc14a7b6481eca

Fixed broken critter art debug message
(caused by commit a34aa64c)

sfall/Modules/BugFixes.cpp
@@ -543,7 +543,7 @@ dudeFix:
 		jnz  equipped;
 		xor  eax, eax;
 equipped:
-		or   cl, 1;                    // reset ZF
+		or   ecx, 1;                   // reset ZF
 		retn;
 	}
 }

sfall/Modules/CritterPoison.cpp
@@ -88,7 +88,7 @@ static __declspec(naked) void critter_check_poison_hack_fix() {
 		retn;
 notDude:
 		call critter_check_poison_fix;
-		or   al, 1; // unset ZF (exit from func)
+		or   eax, 1; // unset ZF (exit from func)
 		retn;
 	}
 }

sfall/Modules/DebugEditor.cpp
@@ -329,12 +329,12 @@ artNotExist:
 
 static __declspec(naked) void art_data_size_hook_check() {
 	using namespace fo;
-	__asm {
-		xor  esi, esi;
-		mov  eax, ebx; // ebx - FID
-		shr  eax, 16;  // al - animation code (ID2)
-		cmp  al, ANIM_walk;
-		cmove ecx, esi;
+	__asm { // ecx = -1
+		xor  eax, eax;
+		mov  edx, ebx; // ebx - FID
+		shr  edx, 16;  // dl - animation code (ID2)
+		cmp  dl, ANIM_walk;
+		cmove eax, ecx; // -1 - skip critter art debug message
 		retn;
 	}
 }
@@ -611,7 +611,6 @@ void DebugEditor::init() {
 	HookCall(0x419B65, art_data_size_hook);
 	// Checks the animation code, if ANIM_walk then skip printing the debug message
 	HookCall(0x419AA0, art_data_size_hook_check);
-	SafeWrite8(0x419B61, CodeType::JumpNZ); // jz > jnz
 
 	// Notifies and prints a debug message about a corrupted proto file to debug.log
 	MakeCall(0x4A1D73, proto_load_pid_hack, 6);