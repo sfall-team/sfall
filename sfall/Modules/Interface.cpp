9cccb2e45b9ac44c3e33fd528cd6f58c92ddd80b
NovaRain <novarain@gmail.com>
2025-05-02 21:28:19 +0800

    Improved the previous commit
    * now the buttons have proper press/release sfx.

--- sfall/Modules/Interface.cpp
+++ sfall/Modules/Interface.cpp
@@ -784,7 +784,7 @@ static void WorldMapInterfacePatch() {
 		HookCall(0x4C2343, wmInterfaceInit_text_font_hook);
 	}
 
-	// Add missing sounds for the buttons on the world map interface (wmInterfaceInit_)
+	// Add missing sounds to the buttons on the world map interface (wmInterfaceInit_)
 	const DWORD wmInterfaceBtnsAddr[] = {
 		0x4C2BF4, // location labels
 		0x4C2BB5, // town/world
@@ -951,20 +951,29 @@ static __declspec(naked) void intface_update_ammo_lights_hack() {
 	}
 }
 
-static __declspec(naked) void gdCustomSelect_hack_done() {
+static __declspec(naked) void gdCustomSelect_hack_buttons() {
 	__asm {
-		mov  eax, 0x503E14; // 'ib1p1xx1'
-		call fo::funcoffs::gsound_play_sfx_file_;
-		mov  ecx, 1; // overwritten engine code
+		cmp  eax, -1;
+		jnz  button;
 		retn;
+button:
+		mov  ebx, fo::funcoffs::gsound_red_butt_release_;
+		mov  edx, fo::funcoffs::gsound_red_butt_press_;
+		call fo::funcoffs::win_register_button_sound_func_;
+		pop  ecx;
+		add  ecx, 24; // offset to next section (0x44A11D, 0x44A16E)
+		jmp  ecx;
 	}
 }
 
-static __declspec(naked) void gdCustomSelect_hack_cancel() {
+static __declspec(naked) void gdCustomSelect_hack_keyretn() {
 	__asm {
+		cmp  dword ptr ds:[FO_VAR_mouse_buttons], 0; // mouse clicked?
+		jne  skip;
 		mov  eax, 0x503E14; // 'ib1p1xx1'
 		call fo::funcoffs::gsound_play_sfx_file_;
-		mov  dword ptr [esp + 0x84 - 0x20 + 4], 1; // overwritten engine code
+skip:
+		mov  ecx, 1; // overwritten engine code
 		retn;
 	}
 }
@@ -1090,9 +1099,10 @@ void Interface::init() {
 		}
 	}
 
-	// Add a click sound to the buttons in component menus under the 'Custom' disposition in the combat control panel
-	MakeCall(0x44A47D, gdCustomSelect_hack_done);
-	MakeCall(0x44A495, gdCustomSelect_hack_cancel, 3);
+	// Add missing sounds to the 'Done' and 'Cancel' buttons in the 'Custom' disposition of the combat control panel
+	const DWORD gdCustomSelBtnsAddr[] = {0x44A100, 0x44A151};
+	MakeCalls(gdCustomSelect_hack_buttons, gdCustomSelBtnsAddr);
+	MakeCall(0x44A47D, gdCustomSelect_hack_keyretn);
 }
 
 void Interface::exit() {
