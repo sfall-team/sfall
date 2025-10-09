06df287b3a5836c1098781e08e5a1790a93ed3d4
NovaRain <novarain@gmail.com>
2025-10-09 19:26:53 +0800

    Updated version number and changelog
    Edits to sfall opcode list.

--- sfall/version.h
+++ sfall/version.h
@@ -24,7 +24,7 @@
 
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 8
-#define VERSION_BUILD 47
-#define VERSION_REV 1
+#define VERSION_BUILD 48
+#define VERSION_REV 0
 
-#define VERSION_STRING "3.8.47.1"
+#define VERSION_STRING "3.8.48"
