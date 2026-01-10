ed1f926d366c4d714af20a29af7223c0d98adfff
NovaRain <novarain@gmail.com>
2026-01-10 20:34:54 +0800

    Expanded the functionality of Fallout1Behavior
    * FO1 initial in-game time is 7:21 rather than 8:24.

--- sfall/Modules/MiscPatches.cpp
+++ sfall/Modules/MiscPatches.cpp
@@ -773,6 +773,7 @@ static void F1EngineBehaviorPatch() {
 		BlockCall(0x4A4343); // disable playing the final movie/credits after the endgame slideshow
 		SafeWrite8(0x477C71, CodeType::JumpShort); // disable halving the weight for power armor items
 		HookCall(0x43F872, endgame_movie_hook); // play movie 10 or 11 based on the player's gender before the credits
+		SafeWrite32(0x4A5201, 264600); // change the initial in-game time to 7:21
 	}
 }
 
