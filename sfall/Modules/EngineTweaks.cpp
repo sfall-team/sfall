7e74abf78f2de5bbb9a3f9e2e40a5d96d8a407b7
NovaRain <novarain@gmail.com>
2026-01-13 14:23:02 +0800

    Added settings about the mirrored shades to tweaks.ini
    Updated the description of Fallout1Behavior.

--- sfall/Modules/EngineTweaks.cpp
+++ sfall/Modules/EngineTweaks.cpp
@@ -18,6 +18,7 @@
 
 #include "..\main.h"
 #include "..\FalloutEngine\Fallout2.h"
+#include "..\Utils.h"
 
 #include "..\Game\items.h"
 
@@ -25,6 +26,36 @@
 
 namespace sfall
 {
+
+static long mirrorShadesPid = fo::PID_MIRROR_SHADES;
+static long mirrorShadesStat = fo::STAT_ch;
+
+static long __fastcall MirrorShadesOverride(fo::GameObject* critter, long stat) {
+	if (stat == mirrorShadesStat) {
+		fo::GameObject* item = fo::func::inven_right_hand(critter);
+		if (item && item->protoId == mirrorShadesPid) return 1;
+		item = fo::func::inven_left_hand(critter);
+		if (item && item->protoId == mirrorShadesPid) return 1;
+	}
+	return 0;
+}
+
+static __declspec(naked) void stat_level_hack_shades() {
+	__asm {
+		push ecx;
+		mov  ecx, ebx; // critter (always obj_dude)
+		mov  edx, esi; // stat
+		call MirrorShadesOverride;
+		pop  ecx;
+		test eax, eax;
+		jz   end;
+		inc  ecx; // stat value +1
+end: // overwritten engine code
+		lea  edi, [ecx + 1];
+		test esi, esi;
+		retn;
+	}
+}
 
 void EngineTweaks::init() {
 	auto tweaksFile = IniReader::GetConfigString("Misc", "TweaksFile", "");
@@ -35,6 +66,15 @@ void EngineTweaks::init() {
 		game::Items::SetHealingPID(0, IniReader::GetInt("Items", "STIMPAK", fo::PID_STIMPAK, cTweaksFile));
 		game::Items::SetHealingPID(1, IniReader::GetInt("Items", "SUPER_STIMPAK", fo::PID_SUPER_STIMPAK, cTweaksFile));
 		game::Items::SetHealingPID(2, IniReader::GetInt("Items", "HEALING_POWDER", fo::PID_HEALING_POWDER, cTweaksFile));
+
+		mirrorShadesPid = IniReader::GetInt("Items", "MIRROR_SHADES_PID", fo::PID_MIRROR_SHADES, cTweaksFile);
+		mirrorShadesStat = clamp<long>(IniReader::GetInt("Items", "MIRROR_SHADES_STAT", fo::STAT_ch, cTweaksFile), -1, fo::STAT_lu);
+		if (mirrorShadesPid != fo::PID_MIRROR_SHADES || mirrorShadesStat != fo::STAT_ch) {
+			MakeCall(0x4AF10A, stat_level_hack_shades);
+			// Disable original handling
+			SafeWrite8(0x4AF1C3, CodeType::Jump);
+			SafeWrite32(0x4AF1C4, 487); // jmp 0x4AF3AF
+		}
 	}
 }
 
