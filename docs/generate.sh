85624adb205a13085a0cc4018bad0ecfd0034f46
NovaRain <novarain@gmail.com>
2026-06-17 09:55:05 +0800

    Reverted to previous commit

--- docs/generate.sh
+++ docs/generate.sh
@@ -10,4 +10,7 @@ cd "$site_dir"
 mkdir -p "$pages_dir"
 ./yaml_to_md.py "$yml_dir/functions.yml" "$yml_dir/hooks.yml" "$pages_dir"
 
+bundle config set --local path 'vendor/bundle'
+bundle update github-pages
+bundle install
 bundle exec jekyll build
