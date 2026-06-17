ed85d0920b69d4a4bd9436c8b958f5424c8d6261
NovaRain <novarain@gmail.com>
2026-06-17 09:29:11 +0800

    Changed Ruby bundler setup

--- docs/generate.sh
+++ docs/generate.sh
@@ -10,7 +10,4 @@ cd "$site_dir"
 mkdir -p "$pages_dir"
 ./yaml_to_md.py "$yml_dir/functions.yml" "$yml_dir/hooks.yml" "$pages_dir"
 
-bundle config set path vendor/bundle
-bundle update github-pages
-bundle install
 bundle exec jekyll build
